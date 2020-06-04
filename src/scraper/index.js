e9384b7af03f356c651a410e9984cecd134e07c1
Nicolas Dupont <npg.dupont@gmail.com>
2020-06-04 18:02:35 +0200

    Scrape a page

--- src/scraper/index.js
+++ src/scraper/index.js
@@ -1,3 +1,19 @@
+import fetch from 'node-fetch';
+
+const headers = new fetch.Headers({
+  'Accept-Language': 'en',
+});
+
+
 export function scrape(url) {
-  return '';
+  return fetch(url, headers)
+    .then(function(response) {
+      if (!response.ok) {
+        throw new Error(`Received HTTP code ${response.status} when trying to fetch '${url}'`);
+      }
+      return response.blob();
+    })
+    .then(function(response) {
+      return response.text();
+    });
 }
