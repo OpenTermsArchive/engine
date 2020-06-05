9f3cee72d1acaa135b5f028dc0df3a2fbf89dd99
Nicolas Dupont <npg.dupont@gmail.com>
2020-06-05 10:26:32 +0200

    Fix path

--- src/history/index.js
+++ src/history/index.js
@@ -2,8 +2,8 @@ import path from 'path';
 import fsApi from 'fs';
 const fs = fsApi.promises;
 
-const ROOT_DIRECTORY = path.resolve('../../data');
-const RAW_DIRECTORY = `${ROOT_DIRECTORY}/raw`;
+export const ROOT_DIRECTORY = path.resolve() + '/data';
+export const RAW_DIRECTORY = `${ROOT_DIRECTORY}/raw`;
 
 
 export async function storeRaw(serviceProviderId, policyType, fileContent) {
