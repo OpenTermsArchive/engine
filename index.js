37fd386d06256a79b8251fb36681b0f52f1f6972
Nicolas Dupont <npg.dupont@gmail.com>
2020-06-09 11:36:33 +0200

    Extract main function in its own file
    Avoid spawning a new node runner instance on each
    scheduled job runs

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-import scrape from './src/scraper/index.js';
-import { persistRaw } from './src/history/index.js';
+import { updateTerms } from './src/index.js';
 
 (async () => {
   try {
-    const result = await scrape('https://www.facebook.com/legal/terms/plain_text_terms');
-    console.log('Start scraping and saving terms of service…')
-    await persistRaw('facebook', 'terms_of_service', result);
+    await updateTerms();
   } catch (e) {
     console.log(e);
   }
