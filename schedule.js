50d7d003226c92c0b6ae857118574ef7f0e92fae
Nicolas Dupont <npg.dupont@gmail.com>
2020-06-09 11:36:41 +0200

    Add log

--- schedule.js
+++ schedule.js
@@ -3,8 +3,9 @@ import schedule from 'node-schedule';
 import { updateTerms } from './src/index.js'
 
 const rule = new schedule.RecurrenceRule();
-rule.minute = 30; // every hour at 30 minutes after the hour
+rule.minute = 30; // at minute 30 past every hour.
 
 console.log('The scheduler is running…');
+console.log('Jobs will run at minute 30 past every hour.');
 
 schedule.scheduleJob(rule, updateTerms);
