461f0cbc5c3e0873932b9cd6b84fd1d6b5a6b207
Nicolas Dupont <npg.dupont@gmail.com>
2020-06-11 11:22:06 +0200

    Log a warning if selector has no match

--- src/sanitizer/index.js
+++ src/sanitizer/index.js
@@ -9,7 +9,13 @@ export default async function sanitize(content, selector) {
 
   if (selector) {
     const { document } = new JSDOM(contentToSanitize).window;
-    contentToSanitize = document.querySelector(selector);
+    const selectedContent = document.querySelector(selector);
+
+    if (selectedContent) {
+      contentToSanitize = selectedContent;
+    } else {
+      console.log(`Warning: the provider selector "${selector}" has no match in the document.`)
+    }
   }
 
   const markdown = turndownService.turndown(contentToSanitize);
