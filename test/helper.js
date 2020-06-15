bd8b6dd0649c0744d1da5545c23567be153326cd
Nicolas Dupont <npg.dupont@gmail.com>
2020-06-15 14:18:45 +0200

    Ensure tests won't modify git history

--- test/helper.js
+++ test/helper.js
@@ -1,2 +1,21 @@
 import dotenv from 'dotenv';
 dotenv.config();
+
+import { git } from '../src/history/git.js';
+
+let lastCommitId;
+let stashResult;
+
+before(async () => {
+  const { latest: { hash } } = await git.log(['-1']);
+  lastCommitId = hash;
+  stashResult = await git.stash(['-u']);
+});
+
+after(async () => {
+  await git.reset(['--hard', lastCommitId]);
+  if (stashResult.includes('No local changes to save')) {
+    return;
+  }
+  return git.stash(['pop']);
+});
