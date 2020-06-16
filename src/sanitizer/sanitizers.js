3e569a25eb2138894b7b2de52e79241dde7397c6
Nicolas Dupont <npg.dupont@gmail.com>
2020-06-16 15:29:37 +0200

    Add removeFacebookHelpButtons sanitizer

--- src/sanitizer/sanitizers.js
+++ src/sanitizer/sanitizers.js
@@ -4,3 +4,13 @@ export function cleanFacebookUrls(document) {
     link.href = link.href.replace(/&h=\S*/, '');
   });
 }
+
+export function removeFacebookHelpButtons(document) {
+  const links = document.querySelectorAll('a[href="#"][id*="-markdown-internal-id"]');
+  links.forEach(link => {
+    // Helps buttons do not have text content
+    if (!link.text) {
+      link.remove();
+    }
+  });
+}
