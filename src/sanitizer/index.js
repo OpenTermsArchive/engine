8577243df6dfca650ccd9245cddc4e5148c43fb2
Nicolas Dupont <npg.dupont@gmail.com>
2020-06-18 09:37:06 +0200

    Change the way sanitizers are passed to `sanitize` function

--- src/sanitizer/index.js
+++ src/sanitizer/index.js
@@ -1,14 +1,19 @@
 import TurndownService from 'turndown';
 import jsdom from 'jsdom';
 
-import * as sanitizers from './sanitizers.js';
-
 const { JSDOM } = jsdom;
 const turndownService = new TurndownService();
 
-export default async function sanitize(content, selector, sanitizationPipeline) {
+export default async function sanitize(content, selector, sanitizationPipeline, additionalSanitizers) {
   let contentToSanitize = content;
-  const { document } = new JSDOM(contentToSanitize).window;
+  let { document } = new JSDOM(contentToSanitize).window;
+
+  if (sanitizationPipeline) {
+    sanitizationPipeline.forEach(sanitizerId => {
+      // Sanitizers work in place
+      additionalSanitizers[sanitizerId](document);
+    });
+  }
 
   if (selector) {
     const selectedContent = document.querySelector(selector);
@@ -18,12 +23,6 @@ export default async function sanitize(content, selector, sanitizationPipeline) 
     } else {
       console.warn(`The provided selector "${selector}" has no match in the document.`)
     }
-  }
-
-  if (sanitizationPipeline) {
-    sanitizationPipeline.forEach(sanitization => {
-      sanitizers[sanitization](document);
-    });
   }
 
   const markdown = turndownService.turndown(contentToSanitize);
