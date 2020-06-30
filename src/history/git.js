dcf9c6fef70aa69ae3f1056ca00591b1ff9ccce2
Nicolas Dupont <npg.dupont@gmail.com>
2020-06-30 10:35:05 +0200

    Fix path

--- src/history/git.js
+++ src/history/git.js
@@ -6,7 +6,7 @@ import simpleGit from 'simple-git';
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
-const DATA_PATH = (process.env.CI ? '' : '../') + '../../CGUs-data';
+const DATA_PATH = (process.env.CI ? '' : '../') + '../../cgus-data';
 
 export const git = simpleGit(path.resolve(__dirname, DATA_PATH));
 
