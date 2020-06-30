e04081990a1657084c4f220e74022c2eb281b954
Nicolas Dupont <npg.dupont@gmail.com>
2020-06-30 17:21:00 +0200

    Use events on CGUs app to notify on errors and updates

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-import updateTerms from './src/index.js';
+import * as notifier from './src/notifier/index.js';
+import CGUs from './src/index.js';
 
 (async () => {
   try {
-    await updateTerms();
+    const app = new CGUs();
+    await app.init();
+
+    notifier.init(app.serviceProviders, app.documentsTypes);
+
+    app.on('sanitizedDocumentChange', notifier.onSanitizedDocumentChange);
+    app.on('documentScrapingError', notifier.onDocumentScrapingError);
+    app.on('applicationError', notifier.onApplicationError);
+
+    await app.updateTerms();
   } catch (e) {
     console.log(e);
   }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import events from 'events';
+
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -9,60 +11,97 @@ import { persistRaw, persistSanitized, pushChanges } from './history/index.js';
 import sanitize from './sanitizer/index.js';
 import getServiceProviders, { getSanitizers } from './service_providers/index.js';
 import { DOCUMENTS_TYPES } from './documents_types.js';
-
-export async function updateServiceProviderDocument({ serviceProviderId, serviceProviderName, document }) {
-  const { documentType, url, contentSelector, sanitizationPipeline } = document;
-  const logPrefix = `[${serviceProviderName}-${DOCUMENTS_TYPES[documentType].name}]`;
-  console.log(`${logPrefix} Scrape '${url}'.`);
-  const content = await scrape(url);
-
-  const { sha: rawSha, filePath: rawFilePath} = await persistRaw(serviceProviderId, documentType, content);
-  if (rawSha) {
-    console.log(`${logPrefix} Save raw file to '${rawFilePath}'.`);
-    console.log(`${logPrefix} Commit raw file in '${rawSha}'.`);
-  } else {
-    console.log(`${logPrefix} No raw changes, didn't commit.`);
+export default class CGUs extends events.EventEmitter {
+  constructor() {
+    super();
+    this.initialized;
+    this.serviceProvidersManifests;
+    this.documentTypes = DOCUMENTS_TYPES;
   }
 
-  const sanitizers = getSanitizers(serviceProviderId);
-  const sanitizedContent = await sanitize(content, contentSelector, sanitizationPipeline, sanitizers);
+  async init() {
+    if (!this.initialized) {
+      this.serviceProvidersManifests = await getServiceProviders();
+      this.initialized = Promise.resolve();
+    }
 
-  const { sha: sanitizedSha, filePath: sanitizedFilePath} = await persistSanitized(serviceProviderId, documentType, sanitizedContent, rawSha);
-  if (sanitizedSha) {
-    console.log(`${logPrefix} Save sanitized file to '${sanitizedFilePath}'.`);
-    console.log(`${logPrefix} Commit sanitized file in '${sanitizedSha}'.`);
-  } else {
-    console.log(`${logPrefix} No changes after sanitization, didn't commit.`);
+    return this.initialized;
   }
-};
 
-export default async function updateTerms() {
-  console.log('Start scraping and saving terms of service…');
+  async updateTerms() {
+    console.log('Start scraping and saving terms of service…');
 
-  const documentUpdatePromises = [];
-  const serviceProvidersManifests = await getServiceProviders();
+    const documentUpdatePromises = [];
 
-  Object.keys(serviceProvidersManifests).forEach((serviceProviderId) => {
-    const { documents, serviceProviderName } = serviceProvidersManifests[serviceProviderId];
+    Object.keys(this.serviceProvidersManifests).forEach((serviceProviderId) => {
+      const { documents, serviceProviderName } = this.serviceProvidersManifests[serviceProviderId];
 
-    Object.keys(documents).forEach((documentType) => {
-      documentUpdatePromises.push(updateServiceProviderDocument({
-        serviceProviderId,
-        serviceProviderName,
-        document: {
-          documentType,
-          ...documents[documentType]
-        }
-      }));
+      Object.keys(documents).forEach((documentType) => {
+        documentUpdatePromises.push(this.updateServiceProviderDocument({
+          serviceProviderId,
+          serviceProviderName,
+          document: {
+            documentType,
+            ...documents[documentType]
+          }
+        }));
+      });
     });
-  });
 
-  await Promise.all(documentUpdatePromises);
+    await Promise.all(documentUpdatePromises);
 
-  if (process.env.NODE_ENV === 'production') {
-    await pushChanges();
-    console.log('・・・・・・・');
-    console.log('Pushed changes to the repository');
-    console.log('______________________________');
+    if (process.env.NODE_ENV === 'production') {
+      await pushChanges();
+      console.log('・・・・・・・');
+      console.log('Pushed changes to the repository');
+      console.log('______________________________');
+    }
+  };
+
+  async updateServiceProviderDocument({ serviceProviderId, serviceProviderName, document }) {
+    const { documentType, url, contentSelector, sanitizationPipeline } = document;
+    const logPrefix = `[${serviceProviderName}-${DOCUMENTS_TYPES[documentType].name}]`;
+    try {
+      console.log(`${logPrefix} Scrape '${url}'.`);
+      let content;
+      try {
+        content = await scrape(url);
+      } catch (error) {
+        console.error(`${logPrefix} Can't scrape url: ${error}`);
+        return this.emit('documentScrapingError', serviceProviderId, documentType, error);
+      }
+
+      const { sha: rawSha, filePath: rawFilePath} = await persistRaw(serviceProviderId, documentType, content);
+
+      console.log(`${logPrefix} Save raw file to '${rawFilePath}'.`);
+
+      if (!rawSha) {
+        return console.log(`${logPrefix} No raw changes, didn't commit.`);
+      }
+
+      console.log(`${logPrefix} Commit raw file in '${rawSha}'.`);
+
+      const sanitizers = getSanitizers(serviceProviderId);
+      const sanitizedContent = await sanitize(content, contentSelector, sanitizationPipeline, sanitizers);
+
+      const { sha: sanitizedSha, filePath: sanitizedFilePath} = await persistSanitized(serviceProviderId, documentType, sanitizedContent, rawSha);
+      if (sanitizedSha) {
+        console.log(`${logPrefix} Save sanitized file to '${sanitizedFilePath}'.`);
+        console.log(`${logPrefix} Commit sanitized file in '${sanitizedSha}'.`);
+        this.emit('sanitizedDocumentChange', serviceProviderId, documentType, sanitizedSha);
+      } else {
+        console.log(`${logPrefix} No changes after sanitization, didn't commit.`);
+      }
+    } catch (error) {
+      console.error(`${logPrefix} Error:`, error);
+      this.emit('applicationError', serviceProviderId, documentType, error);
+    }
   }
-};
+
+  get serviceProviders() {
+    return this.serviceProvidersManifests;
+  }
+  get documentsTypes() {
+    return this.documentTypes;
+  }
+}
