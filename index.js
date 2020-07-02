3d2693c4096e3f95e41b5f51d2b98665e2856dfb
Nicolas Dupont <npg.dupont@gmail.com>
2020-07-02 14:43:30 +0200

    Avoid sending mail when run one time

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-import * as notifier from './src/notifier/index.js';
 import CGUs from './src/index.js';
 
 (async () => {
   try {
     const app = new CGUs();
     await app.init();
-
-    notifier.init(app.serviceProviders, app.documentsTypes);
-
-    app.on('sanitizedDocumentChange', notifier.onSanitizedDocumentChange);
-    app.on('documentScrapingError', notifier.onDocumentScrapingError);
-    app.on('applicationError', notifier.onApplicationError);
 
     await app.updateTerms();
   } catch (e) {
