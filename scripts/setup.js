72ce46a45a5f680728220af0e6db1360df160446
Nicolas Dupont <npg.dupont@gmail.com>
2020-07-06 10:38:02 +0200

    Remove obsolete dependency

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,10 +1,10 @@
-import fs from 'fs';
+import fsApi from 'fs';
+const fs = fsApi.promises;
 import path from 'path';
 
 import config from 'config';
 import inquirer from 'inquirer';
 import simpleGit from 'simple-git';
-import shell from 'shelljs';
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
@@ -14,7 +14,7 @@ const DOWNLOAD_HISTORY_MESSAGE = `Download the entire history of terms of servic
 
 (async () => {
   try {
-    if (fs.existsSync(config.get('history.dataPath'))) {
+    if (fsApi.existsSync(config.get('history.dataPath'))) {
       return console.log(`It seems that the database is already initialized as the "${config.get('history.dataPath')}" directory already exists. Erase that folder first if you’d like to set up a new database.`);
     }
 
@@ -29,8 +29,7 @@ const DOWNLOAD_HISTORY_MESSAGE = `Download the entire history of terms of servic
     }]);
 
     if (answer.history === NEW_HISTORY_MESSAGE) {
-      shell.mkdir(config.get('history.dataPath'));
-      shell.cd(config.get('history.dataPath'));
+      await fs.mkdir(config.get('history.dataPath'), { recursive: true });
       const git = simpleGit(path.resolve(__dirname, '../', config.get('history.dataPath')));
       await git.init();
     } else {
