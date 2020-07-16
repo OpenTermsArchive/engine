01558f79c2fa8cf7b735cd1f1fa3954e2ac5a86f
Nicolas Dupont <npg.dupont@gmail.com>
2020-07-16 16:38:53 +0200

    Use Recorder class in history

--- src/history/index.js
+++ src/history/index.js
@@ -1,8 +1,18 @@
-import { record } from './recorder.js';
-export { pushChanges as publish } from './git.js';
+import path from 'path';
+import config from 'config';
+
+import Recorder from './recorder.js';
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+export const SNAPSHOTS_PATH = path.resolve(__dirname, '../..', config.get('history.snapshotsPath'));
+export const VERSIONS_PATH = path.resolve(__dirname, '../..', config.get('history.versionsPath'));
+
+const snapshotRecorder = new Recorder({ path: SNAPSHOTS_PATH, fileExtension: 'html' });
+const versionRecorder = new Recorder({ path: VERSIONS_PATH, fileExtension: 'md' });
 
 export async function recordSnapshot(serviceId, documentType, content) {
-  return record({
+  return snapshotRecorder.record({
     serviceId,
     documentType,
     content
@@ -14,10 +24,23 @@ export async function recordVersion(serviceId, documentType, content, snapshotId
     throw new Error(`A snapshot ID is required to ensure data consistency for ${serviceId}'s ${documentType}`);
   }
 
-  return record({
+  let id = '';
+  if (config.get('history.publish')) {
+    id += config.get('history.publicSnapshotsRepository')
+      .replace('git@github.com:', 'https://github.com/')
+      .replace('.git', '/commit/');
+  }
+  id += snapshotId;
+
+  return versionRecorder.record({
     serviceId,
     documentType,
     content,
-    snapshotId
+    messageToAppend: `This version was recorded after filtering snapshot ${id}`
   });
+}
+
+export async function publish() {
+  snapshotRecorder.publish();
+  return versionRecorder.publish();
 }
