f63bae3fd91d40faca73d64ee3da2d23fb9c70a8
Adrien Fines <adrien.fines@protonmail.com>
2020-07-20 13:13:54 +0200

    Add tosParent type of document and reformat Skype

--- services/Skype.filters.js
+++ services/Skype.filters.js
@@ -1,4 +1,4 @@
-export function removeIrrelevantModules(document) {
+export function removeIrrelevantModulesPrivacyPolicy(document) {
     const moduleDescriptions = document.querySelectorAll('.divModuleDescription');
     const toDelete = [
         "mainenterprisedeveloperproductsmodule",
@@ -43,46 +43,53 @@ export function removeIrrelevantModules(document) {
         "mainwindowsmixedrealitymodule"
     ];
     moduleDescriptions.forEach(moduleDescription => {
-        let moduleName = element.querySelector('#moduleName');
+        let moduleName = moduleDescription.querySelector('#moduleName');
         if (moduleName) {
             moduleName = moduleName.textContent;
         }
         if (moduleName && toDelete.includes(moduleName)) {
-            element.remove();
+            moduleDescription.remove();
+        }
+    });
+}
+
+export function removeIrrelevantModulesParentTos(document) {
+    const moduleDescriptions = document.querySelectorAll('.divModuleDescription');
+    moduleDescriptions.forEach(moduleDescription => {
+        var name = moduleDescription.querySelector('.FileName');
+        if (name) {
+            name = name.textContent;
+        }
+        if (name.substr(0,2)=="14" && name.substr(0,3)!="14e" && name.substr(0,3)!="14_") {
+            moduleDescription.remove();
+        }
+        if (name=="serviceslist") {
+            moduleDescription.remove();
         }
     });
 }
 
 export function removeNavigationHeaders(document) {
-    document.querySelectorAll('#navigationHeader').forEach(element => element.remove());
+    document.querySelectorAll('#navigationHeader, .navigationHeader').forEach(element => element.remove());
 }
 
 export function removeModuleIDs(document) {
-    document.querySelectorAll('#moduleName').forEach(element => element.remove());
-}
-
-export function cleanSecondaryTos(document) {
-    const divs = document.querySelectorAll('div[class="divModuleDescription"]');
-    divs.forEach(element => {
-        var name = element.querySelector('span[class="FileName"]');
-        if(name){name = name.textContent;}
-        if(name.substr(0,2)=="14" && name.substr(0,3)!="14e" && name.substr(0,3)!="14_"){element.remove();}
-        if(name=="serviceslist"){element.remove();}
-    });
-
-    const navDuplicates = document.querySelectorAll('span[class="navigationHeader"]');
-    const moduleDuplicates = document.querySelectorAll('span[class="FileName"]');
-    const notuseful = document.querySelectorAll('div[class="displayNone"]');
-    navDuplicates.forEach(element => element.remove());
-    moduleDuplicates.forEach(element => element.remove());
-    notuseful.forEach(element => element.remove());
+    document.querySelectorAll('#moduleName, .FileName').forEach(element => element.remove());
 }
 
 export function removeInvisibleText(document) {
-    const notDisplayed = document.querySelectorAll('div[class="displayNone"]');
-    const summaries = document.querySelectorAll('div[class="printsummary"]');
-    const fulltexts = document.querySelectorAll('div[class="printDetailedContent"]');
+    const notDisplayed = document.querySelectorAll('.displayNone');
+    const summaries = document.querySelectorAll('.printsummary');
+    const fulltexts = document.querySelectorAll('.printDetailedContent');
     notDisplayed.forEach(element => element.remove());
-    summaries.forEach(summary =>  {if (summary.textContent=="Summary") {summary.remove();}});
-    fulltexts.forEach(fulltext => {if (fulltext.textContent=="Full text") {fulltext.remove();}});
+    summaries.forEach(summary =>  {
+        if (summary.textContent=="Summary") {
+            summary.remove();
+        }
+    });
+    fulltexts.forEach(fulltext => {
+        if (fulltext.textContent=="Full text") {
+            fulltext.remove();
+        }
+    });
 }
