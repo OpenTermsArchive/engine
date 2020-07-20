2eb085039f7ee42b580146bf99bc1b81dc4e4c42
Matti Schneider <matti.schneider@diplomatie.gouv.fr>
2020-07-20 10:09:24 +0200

    Refactor multiple selection in filters
    Document expectation of returning entire document when no selector is provided

--- src/filter/index.js
+++ src/filter/index.js
@@ -4,9 +4,9 @@ import jsdom from 'jsdom';
 const { JSDOM } = jsdom;
 const turndownService = new TurndownService();
 
+
 export default async function filter(content, selector, filterNames, filterFunctions) {
-  let contentToFilter = content;
-  let { document: webPageDOM } = new JSDOM(contentToFilter).window;
+  let { document: webPageDOM } = new JSDOM(content).window;
 
   if (filterNames) {
     filterNames.forEach(filterName => {
@@ -15,17 +15,14 @@ export default async function filter(content, selector, filterNames, filterFunct
     });
   }
 
+  let selectedContent = [ webPageDOM ];
+
   if (selector) {
-    const selectedContent = webPageDOM.querySelectorAll(selector);
-    if (selectedContent) {
-      contentToFilter = selectedContent;
-    } else {
-      console.warn(`The provided selector "${selector}" has no match in the web page.`)
+    selectedContent = Array.from(webPageDOM.querySelectorAll(selector));
+    if (!selectedContent.length) {
+      console.warn(`The provided selector "${selector}" has no match in the web page.`);
     }
   }
 
-  let markdown = "";
-  contentToFilter.forEach(next_content => {markdown += turndownService.turndown(next_content)+"\n";});
-  const markdown_final = markdown;
-  return markdown_final;
+  return selectedContent.map(domFragment => turndownService.turndown(domFragment)).join('\n');
 }
