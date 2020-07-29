5d2ff4e438e63acb312c1b1bdaf926fa36bf4a3b
Adrien Fines <adrien.fines@protonmail.com>
2020-07-29 17:44:52 +0200

    Refactor

--- services/TikTok.filters.js
+++ services/TikTok.filters.js
@@ -1,50 +1,50 @@
 export function selectOnlyEuropeanTOS(document) {
-  //Select titles for juridictions
+	//Select titles for juridictions
 	const titleAmerican = document.querySelector('#terms-us');
 	const titleEuropean = document.querySelector('#terms-eea');
-	const titleIndia =  document.querySelector('#terms-in')
-  //Delete useless paragraphs
+	const titleIndia = document.querySelector('#terms-in')
+	//Delete useless paragraphs
 	let currentNode = titleAmerican.previousElementSibling;
-	while(currentNode){
+	while (currentNode) {
 		let toRemove = currentNode;
 		currentNode = currentNode.previousElementSibling;
 		toRemove.remove();
 	}
-  //Delete others jurisdictions
+	//Delete others jurisdictions
 	currentNode = titleAmerican;
-	while(currentNode != titleEuropean){
+	while (currentNode != titleEuropean) {
 		let toRemove = currentNode;
 		currentNode = currentNode.nextElementSibling;
 		toRemove.remove();
 	}
 	currentNode = titleIndia;
-	while(currentNode){
+	while (currentNode) {
 		let toRemove = currentNode;
 		currentNode = currentNode.nextElementSibling;
 		toRemove.remove();
 	}
 }
 export function selectOnlyEuropeanPrivacyPolicy(document) {
-  //Select titles for juridictions
+	//Select titles for juridictions
 	const titleAmerican = document.querySelector('#privacy-us');
 	const titleEuropean = document.querySelector('#privacy-eea');
 	const titleOther = document.querySelector('#privacy-row')
-  //Delete useless paragraphs
+	//Delete useless paragraphs
 	let currentNode = titleAmerican.previousElementSibling;
-	while(currentNode){
+	while (currentNode) {
 		let toRemove = currentNode;
 		currentNode = currentNode.previousElementSibling;
 		toRemove.remove();
 	}
-  //Delete others jurisdictions
+	//Delete others jurisdictions
 	currentNode = titleAmerican;
-	while(currentNode != titleEuropean){
+	while (currentNode != titleEuropean) {
 		let toRemove = currentNode;
 		currentNode = currentNode.nextElementSibling;
 		toRemove.remove();
 	}
 	currentNode = titleOther;
-	while(currentNode){
+	while (currentNode) {
 		let toRemove = currentNode;
 		currentNode = currentNode.nextElementSibling;
 		toRemove.remove();
