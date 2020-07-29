9ac9b1feed80207de7d7b9ffaa53599ae251b52f
Adrien Fines <adrien.fines@protonmail.com>
2020-07-29 17:44:52 +0200

    Refactor

--- services/LibraryThing.filters.js
+++ services/LibraryThing.filters.js
@@ -5,32 +5,32 @@ export function selectOnlyTOS(document) {
 	const titleMobilePP = Array.from(document.querySelectorAll('h3')).filter(title => title.textContent == 'Mobile Apps (Android and iOS)')[0];
 
 	let currentNode = titlePP;
-	while(currentNode != titleTOS){
+	while (currentNode != titleTOS) {
 		let toRemove = currentNode;
 		currentNode = currentNode.nextElementSibling;
 		toRemove.remove();
 	}
 	currentNode = titleMobilePP;
-	while(currentNode){
+	while (currentNode) {
 		let toRemove = currentNode;
 		currentNode = currentNode.nextElementSibling;
 		toRemove.remove();
 	}
 }
 
-export function selectOnlyPrivacyPolicy(document){
+export function selectOnlyPrivacyPolicy(document) {
 	const h2Titles = document.querySelectorAll('h2');
 	const titleCR = h2Titles[1];
 	const titleMobilePP = Array.from(document.querySelectorAll('h3')).filter(title => title.textContent == 'Mobile Apps (Android and iOS)')[0];
 	const titleLast = h2Titles[3];
 	let currentNode = titleCR;
-	while(currentNode != titleMobilePP){
+	while (currentNode != titleMobilePP) {
 		let toRemove = currentNode;
 		currentNode = currentNode.nextElementSibling;
 		toRemove.remove();
 	}
 	currentNode = titleLast;
-	while(currentNode){
+	while (currentNode) {
 		let toRemove = currentNode;
 		currentNode = currentNode.nextElementSibling;
 		toRemove.remove();
