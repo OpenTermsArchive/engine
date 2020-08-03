38d3bcb97ea53b648f257e56e20a890b8ddf8803
Nicolas Dupont <npg.dupont@gmail.com>
2020-08-03 14:47:23 +0200

    Fix validate command

--- scripts/validation/service.schema.js
+++ scripts/validation/service.schema.js
@@ -1,57 +1,61 @@
 import { TYPES } from '../../src/types.js';
+
 const AVAILABLE_TYPES_NAME = Object.keys(TYPES);
 
 const documentsProperties = () => {
   const result = {};
-  AVAILABLE_TYPES_NAME.forEach((type) => result[type] = { "$ref": "#/definitions/document" });
+  AVAILABLE_TYPES_NAME.forEach(type => result[type] = { $ref: '#/definitions/document' });
   return result;
 };
 
 const schema = {
-  "type": "object",
-  "additionalProperties": false,
-  "title": "Service declaration",
-  "required": [
-    "name",
-    "documents"
+  type: 'object',
+  additionalProperties: false,
+  title: 'Service declaration',
+  required: [
+    'name',
+    'documents'
   ],
-  "properties": {
-    "name": {
-      "type": "string",
-      "title": "Service public name",
-      "examples": [
-        "Facebook"
+  properties: {
+    name: {
+      type: 'string',
+      title: 'Service public name',
+      examples: [
+        'Facebook'
       ]
     },
-    "documents": {
-      "type": "object",
-      "properties": documentsProperties(),
-      "propertyNames": {
-        "enum": AVAILABLE_TYPES_NAME,
+    documents: {
+      type: 'object',
+      properties: documentsProperties(),
+      propertyNames: {
+        enum: AVAILABLE_TYPES_NAME,
       }
     }
   },
-  "definitions": {
-    "document": {
-      "type": "object",
-      "additionalProperties": false,
-      "required": [
-        "location",
-        "contentSelector"
+  definitions: {
+    document: {
+      type: 'object',
+      additionalProperties: false,
+      required: [
+        'location',
+        'contentSelector'
       ],
-      "properties": {
-        "location": {
-          "type": "string",
-          "format": "uri",
-          "description": "The URL where the document can be found"
+      properties: {
+        location: {
+          type: 'string',
+          format: 'uri',
+          description: 'The URL where the document can be found'
         },
-        "contentSelector": {
-          "type": "string",
-          "pattern": "^.+$",
-          "description": "The CSS selector that targets the meaningful part of the document, excluding elements such as headers, footers and navigation"
+        contentSelector: {
+          type: [ 'string', 'object', 'array' ],
+          description: 'The content selector that targets the meaningful part of the document, excluding elements such as headers, footers and navigation'
         },
-        "filters": {
-          "type": "array"
+        filters: {
+          type: 'array'
+        },
+        removeElements: {
+          type: [ 'string', 'object', 'array' ],
+          description: 'The content selector that targets element to exclude'
         }
       }
     }
