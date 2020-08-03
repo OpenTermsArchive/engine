06829a27c0ab7ddb895bfa58c26d09bb4fecb189
Nicolas Dupont <npg.dupont@gmail.com>
2020-08-03 14:47:23 +0200

    Fix validate command

--- scripts/validation/validate.js
+++ scripts/validation/validate.js
@@ -10,12 +10,12 @@ import jsonSourceMap from 'json-source-map';
 import fetch from '../../src/fetcher/index.js';
 import filter from '../../src/filter/index.js';
 import loadServiceDeclarations from '../../src/loader/index.js';
-import serviceSchema from './service.schema.js'
+import serviceSchema from './service.schema.js';
 import { TYPES } from '../../src/types.js';
 
 const fs = fsApi.promises;
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
-const expect = chai.expect;
+const { expect } = chai;
 const AVAILABLE_TYPE_NAMES = Object.keys(TYPES);
 const rootPath = path.join(__dirname, '../..');
 
@@ -30,7 +30,7 @@ describe('Service validation', () => {
     if (!service) {
       throw new Error(`There is no service declared with id ${serviceId}`);
     }
-  })
+  });
 
   describe(serviceId, () => {
     it('has a valid declaration', async () => {
@@ -39,40 +39,40 @@ describe('Service validation', () => {
     });
 
     AVAILABLE_TYPE_NAMES.forEach(type => {
-      describe(TYPES[type].name, function() {
-        before(function() {
+      describe(TYPES[type].name, () => {
+        before(function () {
           if (!service.documents[type]) {
             console.log('      (Tests skipped for this document type as it is not declared for this service)');
             this.skip();
           }
         });
 
-        it('has fetchable URL', async function() {
+        it('has fetchable URL', async function () {
           this.timeout(10000);
 
-          const { location } = service.documents[type];
+          const { location } = service.documents[type];
           await fetch(location);
         });
 
-        it('has a selector that matches an element in the web page', async function() {
+        it('has a selector that matches an element in the web page', async function () {
           this.timeout(10000);
 
-          const { contentSelector, location } = service.documents[type];
-          let { document: webPageDOM } = new jsdom.JSDOM(await fetch(location)).window;
-          const selectedContent = webPageDOM.querySelector(contentSelector);
-          expect(selectedContent).to.exist;
+          const { contentSelector, location, removeElements, filters } = service.documents[type];
+          const content = await fetch(location);
+          const filtredContent = await filter(content, contentSelector, removeElements, filters, service.filters);
+          expect(filtredContent).to.exist;
         });
 
         context('When fetched and filtered twice in a row', () => {
-          it('has consistent filtered content', async function() {
+          it('has consistent filtered content', async function () {
             this.timeout(10000);
 
-            const { contentSelector, location, filters } = service.documents[type];
+            const { contentSelector, location, removeElements, filters } = service.documents[type];
             const filteredContent = [];
 
             for (let i = 0; i < 2; i++) {
               const content = await fetch(location);
-              filteredContent[i] = await filter(content, contentSelector, location, filters, service.filters);
+              filteredContent[i] = await filter(content, location, contentSelector, removeElements, filters, service.filters);
             }
 
             expect(filteredContent[0]).to.equal(filteredContent[1]);
@@ -82,7 +82,6 @@ describe('Service validation', () => {
     });
   });
 });
-
 
 const validator = new Ajv({
   allErrors: true,
@@ -98,10 +97,10 @@ function assertValid(schema, subject) {
     const sourceMap = jsonSourceMap.stringify(subject, null, 2);
     const jsonLines = sourceMap.json.split('\n');
     validator.errors.forEach(error => {
-      errorMessage += '\n\n' + validator.errorsText([ error ]);
-      let errorPointer = sourceMap.pointers[error.dataPath];
+      errorMessage += `\n\n${validator.errorsText([error])}`;
+      const errorPointer = sourceMap.pointers[error.dataPath];
       if (errorPointer) {
-        errorMessage += '\n> ' + jsonLines.slice(errorPointer.value.line, errorPointer.valueEnd.line).join('\n> ');
+        errorMessage += `\n> ${jsonLines.slice(errorPointer.value.line, errorPointer.valueEnd.line).join('\n> ')}`;
         errorPointers.add(errorPointer);
       } else {
         errorMessage += ' (in entire file)\n';
