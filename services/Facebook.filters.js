5017140353e83a5466997edbc8e3dd51535554a5
Adrien Fines <adrien.fines@protonmail.com>
2020-08-05 17:41:55 +0200

    Remove empty links and solve numbering (partly)

--- services/Facebook.filters.js
+++ services/Facebook.filters.js
@@ -15,11 +15,18 @@ export function removeImgFromCookiesPolicy(document) {
 export function removeReturnToTopButtons(document) {
   document.querySelectorAll('._t3o').forEach(element => element.remove());
 }
-
 export function cleanUrls(document) {
+  Array.from(document.querySelectorAll('[href="#"]')).map(link => link.removeAttribute('href'));
   const links = document.querySelectorAll('[href*="https://l.facebook.com/l.php?"],[href*="http://l.facebook.com/l.php?"]');
 
   links.forEach(link => {
     link.href = link.href.replace(/&h=\S*/, '');
   });
+}
+
+export function numberListCorrectly(document) {
+  document.querySelectorAll('ol')
+    .forEach(listToClean => Array.from(listToClean.children)
+      .filter(element => element.tagName !== 'LI')
+      .map(element => element.remove()));
 }
--- services/FacebookPayments.filters.js
+++ services/FacebookPayments.filters.js
@@ -1,14 +1,7 @@
+export { removeReturnToTopButtons } from './Facebook.filters.js';
+export { cleanUrls } from './Facebook.filters.js';
+export { numberListCorrectly } from './Facebook.filters.js';
+
 export function removeHelpButtons(document) {
   document.querySelectorAll('._254').forEach(img => img.remove());
-}
-export function removeReturnToTopButtons(document) {
-  document.querySelectorAll('._t3o').forEach(element => element.remove());
-}
-
-export function cleanUrls(document) {
-  const links = document.querySelectorAll('[href*="https://l.facebook.com/l.php?"],[href*="http://l.facebook.com/l.php?"]');
-
-  links.forEach(link => {
-    link.href = link.href.replace(/&h=\S*/, '');
-  });
 }
