40e145ebe2b46cf1622e53595af1062430706ba2
Adrien Fines <adrien.fines@protonmail.com>
2020-08-05 17:41:55 +0200

    Remove empty links and solve numbering (partly)

--- services/FacebookAds.filters.js
+++ services/FacebookAds.filters.js
@@ -1,17 +1,2 @@
-export function removeHelpButtons(document) {
-  const imgs = document.querySelectorAll('img[src*="https://scontent"]');
-  imgs.forEach(img => {
-    const parent = img.parentNode;
-    if (parent.tagName === 'A' && !parent.text) {
-      parent.remove();
-    }
-  });
-}
-
-export function cleanUrls(document) {
-  const links = document.querySelectorAll('[href*="https://l.facebook.com/l.php?"],[href*="http://l.facebook.com/l.php?"]');
-
-  links.forEach(link => {
-    link.href = link.href.replace(/&h=\S*/, '');
-  });
-}
+export { removeHelpButtons } from './Facebook.filters.js';
+export { cleanUrls } from './Facebook.filters.js';
