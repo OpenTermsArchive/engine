497508af1cdad787c7ccc2ce43ae6917e89b3c9f
Adrien Fines <adrien.fines@protonmail.com>
2020-08-06 09:53:19 +0200

    refactor

--- services/Twitter.filters.js
+++ services/Twitter.filters.js
@@ -1,9 +1,9 @@
 export function removeNotDisplayedElements(document) {
   document.querySelectorAll('[style="display: none;"]').forEach(element => element.remove());
 }
-export function removeBanner(document){
-	document.querySelector('.ap01-breadcrumb').remove();
+export function removeBanner(document) {
+  document.querySelector('.ap01-breadcrumb').remove();
 }
-export function removeScripts(document){
-	document.querySelectorAll('script').forEach(element => element.remove());
+export function removeScripts(document) {
+  document.querySelectorAll('script').forEach(element => element.remove());
 }
