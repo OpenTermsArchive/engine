8676f972ee2ad0b459a335a04e3d82acf4ba1270
Michiel de Jong <michiel@unhosted.org>
2020-08-07 09:42:04 +0200

    Some new types

--- src/types.js
+++ src/types.js
@@ -1,11 +1,71 @@
 export const TYPES = {
   tos: {
-    name: 'Terms of service',
+    name: 'Terms of Service',
     fileName: 'terms_of_service'
+  },
+  website_tos: {
+    name: 'Website Terms of Service',
+    fileName: 'website_terms_of_service'
+  },
+  user_agreement: {
+    name: 'User Agreement',
+    fileName: 'user_agreement'
+  },
+  visitor_agreement: {
+    name: 'Visitor Agreement',
+    fileName: 'visitor_agreement'
+  },
+  vendor_tos: {
+    name: 'Vendor Terms of Service',
+    fileName: 'vendor_terms_of_service'
+  },
+  tou: {
+    name: 'Terms of Use',
+    fileName: 'terms_of_use'
+  },
+  acceptableUsePolicy: {
+    name: 'Acceptable Use Policy',
+    fileName: 'acceptable_use_policy'
+  },
+  termsAndConditions: {
+    name: 'Terms and Conditions',
+    fileName: 'terms_and_conditions'
+  },
+  termsAndPrivacyPolicy: {
+    name: 'Terms and Privacy Policy',
+    fileName: 'terms_and_privacy_policy'
+  },
+  termsOfUseAndConditionsOfUse: {
+    name: 'Terms of Use and Conditions of Use',
+    fileName: 'terms_of_use_and_conditions_of_use'
+  },
+  conditionsOfUse: {
+    name: 'Conditions of Use',
+    fileName: 'conditions_of_use'
+  },
+  termsOfSale: {
+    name: 'Terms of Sale',
+    fileName: 'terms_of_sale'
   },
   privacy: {
     name: 'Privacy Policy',
     fileName: 'privacy_policy'
+  },
+  privacyFull: {
+    name: 'Full Privacy Policy',
+    fileName: 'full_privacy_policy'
+  },
+  dataPolicy: {
+    name: 'Data Policy',
+    fileName: 'data_policy'
+  },
+  dataUsePolicy: {
+    name: 'Data Use Policy',
+    fileName: 'data_use_policy'
+  },
+  privacyStatement: {
+    name: 'Privacy Statement',
+    fileName: 'privacy_statement'
   },
   devAgreement: {
     name: 'Developer Agreement',
@@ -19,17 +79,45 @@ export const TYPES = {
     name: 'Community Guidelines',
     fileName: 'community_guidelines'
   },
+  customerPrivacyPolicy: {
+    name: 'Costomer Privacy Policy',
+    fileName: 'costumer_privacy_policy'
+  },
+  DMCA: {
+    name: 'DMCA',
+    fileName: 'dmca'
+  },
+  EULA: {
+    name: 'EULA',
+    fileName: 'eula'
+  },
   brandGuidelines: {
     name: 'Brand Guidelines',
     fileName: 'brand_guidelines'
+  },
+  geoGuidelines: {
+    name: 'Geo Guidelines',
+    fileName: 'geo_guidelines'
   },
   reviewGuidelines: {
     name: 'Review Guidelines',
     fileName: 'review_guidelines'
   },
+  cookiePolicy: {
+    name: 'Cookie Policy',
+    fileName: 'cookie_policy'
+  },
+  cookieAndAdPolicy: {
+    name: 'Cookie and Ad Policy',
+    fileName: 'cookie_and_ad_policy'
+  },
   cookiesPolicy: {
     name: 'Cookies Policy',
     fileName: 'cookies_policy'
+  },
+  copyright: {
+    name: 'Copyright',
+    fileName: 'copyright'
   },
   commercialTerms: {
     name: 'Commercial Terms',
@@ -42,5 +130,17 @@ export const TYPES = {
   lawEnforcementGuidelines: {
     name: 'Law Enforcement Guidelines',
     fileName: 'law_enforcement_guidelines'
-  }
+  },
+  consumerTermsOfSale: {
+    name: 'Consumer Terms of Sale',
+    fileName: 'consumer_terms_of_sale'
+  },
+  securityAndPrivacy: {
+    name: 'Security and Privacy',
+    fileName: 'security_and_privacy'
+  },
+  vulnerabilityDisclosureProgram: {
+    name: 'Vulnerability Disclosure Program',
+    fileName: 'vulnerability_disclosure_program'
+  },
 };
