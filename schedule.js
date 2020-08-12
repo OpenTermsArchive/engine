67af6e3f8943005777866d87ac5b53fcbcb2151d
Nicolas Dupont <npg.dupont@gmail.com>
2020-08-12 11:03:26 +0200

    Add step to refilter before tracking changes

--- schedule.js
+++ schedule.js
@@ -13,6 +13,8 @@ import CGUs from './src/index.js';
 
     const app = new CGUs();
     await app.init();
+    await app.refilterAndRecordAll();
+
     const notifier = new Notifier(app.serviceDeclarations);
 
     let delayedVersionNotificationsParams = [];
