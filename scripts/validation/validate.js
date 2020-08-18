78f19f1197c0476d44277c1573aa4347a0f30d6f
Michiel de Jong <michiel@unhosted.org>
2020-08-18 09:37:07 +0200

    reuse validation code

--- scripts/validation/validate.js
+++ scripts/validation/validate.js
@@ -1,145 +1,80 @@
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable func-names */
+
 import fsApi from 'fs';
 import path from 'path';
 
-import Ajv from 'ajv';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import chai from 'chai';
 import config from 'config';
-import jsonSourceMap from 'json-source-map';
+import { createRequire } from 'module';
 
-import fetch from '../../src/fetcher/index.js';
-import filter from '../../src/filter/index.js';
 import loadServiceDeclarations from '../../src/loader/index.js';
 import serviceSchema from './service.schema.js';
+import { assertValid, validateDocument } from './validator.js';
+
+const require = createRequire(import.meta.url);
+const TYPES = require('../../src/types.json');
 
 const fs = fsApi.promises;
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 const { expect } = chai;
+const AVAILABLE_TYPE_NAMES = Object.keys(TYPES);
 const rootPath = path.join(__dirname, '../..');
-const MIN_DOC_LENGTH = 100;
+const SERVICE_DECLARATIONS_PATH = path.resolve(__dirname, '../../', config.get('serviceDeclarationsPath'));
 
-let serviceDeclarations;
-(async () => {
-  try {
-    serviceDeclarations = await loadServiceDeclarations(path.join(rootPath, config.get('serviceDeclarationsPath')));
+describe('Services validation', async () => {
+  const serviceId = process.argv.slice(process.argv.indexOf('--serviceId'))[1];
+  const schemaOnly = process.argv.indexOf('--schema-only') != -1;
+  const serviceIds = fsApi.readdirSync(SERVICE_DECLARATIONS_PATH).filter(filename => path.extname(filename) === '.json').map(filename => path.basename(filename, '.json'));
+  const servicesToValidate = serviceId ? [ serviceId ] : serviceIds;
 
-    describe('Services validation', async () => {
-      const serviceId = process.argv.slice(process.argv.indexOf('--serviceId'))[1];
-      const schemaOnly = process.argv.indexOf('--schema-only') != -1;
-      const serviceIds = Object.keys(serviceDeclarations);
+  servicesToValidate.forEach(serviceId => {
+    let service;
 
-      const servicesToValidate = serviceId ? [ serviceId ] : serviceIds;
+    before(async () => {
+      const serviceDeclarations = await loadServiceDeclarations(path.join(rootPath, config.get('serviceDeclarationsPath')));
+      service = serviceDeclarations[serviceId];
 
-      servicesToValidate.forEach(serviceId => {
-        const service = serviceDeclarations[serviceId];
-
-        if (!service) {
-          console.error(`There is no service declared with id "${serviceId}"`);
-          process.exit();
-        }
-
-        describe(serviceId, () => {
-          it('has a valid declaration', async () => {
-            const declaration = JSON.parse(await fs.readFile(path.join(rootPath, config.get('serviceDeclarationsPath'), `${serviceId}.json`)));
-            assertValid(serviceSchema, declaration);
-          });
-
-          if (!schemaOnly) {
-            Object.keys(service.documents).forEach(type => {
-              describe(type, () => {
-                let content;
-                let filteredContent;
-
-                it('has fetchable URL', async function () {
-                  this.timeout(10000);
-
-                  const { fetch: location } = service.documents[type];
-                  content = await fetch(location);
-                });
-
-                it('has a selector that matches an element in the web page', async function () {
-                  if (!content) {
-                    console.log('      (Tests skipped as url is not fetchable)');
-                    this.skip();
-                  }
-
-                  filteredContent = await filter(content, service.documents[type], service.filters);
-                  expect(filteredContent).to.not.be.empty;
-                });
-
-                it(`has a resulting filtered content with at least ${MIN_DOC_LENGTH}`, async function () {
-                  if (!content) {
-                    console.log('      (Tests skipped as url is not fetchable)');
-                    this.skip();
-                  }
-
-                  if (!filteredContent) {
-                    console.log('      (Tests skipped as content cannot be filtered)');
-                    this.skip();
-                  }
-
-                  expect(filteredContent.length).to.be.greaterThan(MIN_DOC_LENGTH);
-                });
-
-                context('When fetched and filtered twice in a row', () => {
-                  it('has consistent filtered content', async function () {
-                    if (!content) {
-                      console.log('      (Tests skipped as url is not fetchable)');
-                      this.skip();
-                    }
-
-                    if (!filteredContent) {
-                      console.log('      (Tests skipped as content cannot be filtered)');
-                      this.skip();
-                    }
-
-                    this.timeout(10000);
-
-                    const { fetch: location } = service.documents[type];
-                    const secondContent = await fetch(location);
-                    const secondFilteredContent = await filter(secondContent, service.documents[type], service.filters);
-
-                    expect(secondFilteredContent).to.equal(filteredContent);
-                  });
-                });
-              });
-            });
-          }
-        });
-      });
-    });
-
-    run();
-  } catch (error) {
-    console.error(error);
-  }
-})();
-
-const validator = new Ajv({
-  allErrors: true,
-  jsonPointers: true,
-});
-
-function assertValid(schema, subject) {
-  const valid = validator.validate(schema, subject);
-
-  if (!valid) {
-    const errorPointers = new Set();
-    let errorMessage = '';
-    const sourceMap = jsonSourceMap.stringify(subject, null, 2);
-    const jsonLines = sourceMap.json.split('\n');
-    validator.errors.forEach(error => {
-      errorMessage += `\n\n${validator.errorsText([error])}`;
-      const errorPointer = sourceMap.pointers[error.dataPath];
-      if (errorPointer) {
-        errorMessage += `\n> ${jsonLines.slice(errorPointer.value.line, errorPointer.valueEnd.line).join('\n> ')}`;
-        errorPointers.add(errorPointer);
-      } else {
-        errorMessage += ' (in entire file)\n';
+      if (!service) {
+        throw new Error(`There is no service declared with id ${serviceId}`);
       }
     });
 
-    errorMessage += `\n\n${errorPointers.size} features have errors in total`;
+    describe(serviceId, () => {
+      it('has a valid declaration', async () => {
+        const declaration = JSON.parse(await fs.readFile(path.join(rootPath, config.get('serviceDeclarationsPath'), `${serviceId}.json`)));
+        assertValid(serviceSchema, declaration);
+      });
 
-    throw new Error(errorMessage);
-  }
-}
+      if (!schemaOnly) {
+        AVAILABLE_TYPE_NAMES.forEach(type => {
+          describe(type, () => {
+            let validationResults;
+            before(async function () {
+              this.timeout(11000); // should be longer than the timeout built into validateDocument
+
+              if (!service.documents[type]) {
+                console.log('      (Tests skipped for this document type as it is not declared for this service)');
+                this.skip();
+              }
+              validationResults = await validateDocument(service.documents[type], service.filters);
+            });
+
+            it('has fetchable URL', async function () {
+              expect(validationResults.fetchable).to.be.true;
+            });
+
+            it('has a selector that matches an element in the web page', async function () {
+              expect(validationResults.selectorMatchesAnElement).to.be.true;
+            });
+
+            it('has consistent filtered content', async function () {
+              expect(validationResults.hasConsistentFilteredContent).to.be.true;
+            });
+          });
+        });
+      }
+    });
+  });
+});
