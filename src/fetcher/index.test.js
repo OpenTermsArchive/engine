a032bc96c9997ed1d16a7adcc78891ec1a884aaa
Nicolas Dupont <npg.dupont@gmail.com>
2020-08-20 10:00:50 +0200

    Allow `fetcher` returning a blob when the filetype is not text

--- src/fetcher/index.test.js
+++ src/fetcher/index.test.js
@@ -9,11 +9,15 @@ const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 const { expect } = chai;
 const facebookTermsHTML = fs.readFileSync(path.resolve(__dirname, '../../test/fixtures/facebook_terms_raw.html'), { encoding: 'utf8' });
+const termsPDF = fs.readFileSync(path.resolve(__dirname, '../../test/fixtures/terms.pdf'));
 
-nock('https://www.facebook.com', {
-  reqheaders: { 'Accept-Language': 'en' }
-}).get('/terms.php')
-  .reply(200, facebookTermsHTML);
+nock('https://www.facebook.com', { reqheaders: { 'Accept-Language': 'en' } })
+  .get('/terms.php')
+  .reply(200, facebookTermsHTML, { 'Content-Type': 'text/html' });
+
+nock('https://testdomain.com', { reqheaders: { 'Accept-Language': 'en' } })
+  .get('/terms.pdf')
+  .reply(200, termsPDF, { 'Content-Type': 'application/pdf' });
 
 nock('https://not.available.document.com')
   .get('/')
@@ -36,6 +40,25 @@ describe('Fetcher', () => {
           return;
         }
         expect.fail('No error was thrown');
+      });
+    });
+
+    context('when url targets a PDF file', () => {
+      let result;
+      before(async () => {
+        result = await fetch('https://testdomain.com/terms.pdf');
+      });
+
+      it('returns a blob', async () => {
+        expect(result.constructor.name).to.equal('Blob');
+      });
+
+      it('returns a blob with the file type', async () => {
+        expect(result.type).to.equal('application/pdf');
+      });
+
+      it('returns a blob with the file content', async () => {
+        expect(Buffer.from(await result.arrayBuffer()).equals(termsPDF)).to.be.true;
       });
     });
   });
