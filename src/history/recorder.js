74751050e305781979d543c46746cb5d05986009
Nicolas Dupont <npg.dupont@gmail.com>
2020-08-20 10:46:03 +0200

    Allow dynamically specify file extension to recorder

--- src/history/recorder.js
+++ src/history/recorder.js
@@ -11,8 +11,8 @@ export default class Recorder {
     this.git = new Git(this.path);
   }
 
-  async record({ serviceId, documentType, content, changelog }) {
-    const filePath = await this.save({ serviceId, documentType, content });
+  async record({ serviceId, documentType, content, changelog, fileExtension }) {
+    const filePath = await this.save({ serviceId, documentType, content, fileExtension });
     const sha = await this.commit(filePath, changelog);
 
     return {
@@ -21,14 +21,14 @@ export default class Recorder {
     };
   }
 
-  async save({ serviceId, documentType, content }) {
+  async save({ serviceId, documentType, content, fileExtension }) {
     const directory = `${this.path}/${serviceId}`;
 
     if (!await fileExists(directory)) {
       await fs.mkdir(directory, { recursive: true });
     }
 
-    const filePath = this.getPathFor(serviceId, documentType);
+    const filePath = this.getPathFor(serviceId, documentType, fileExtension);
 
     await fs.writeFile(filePath, content);
 
@@ -53,21 +53,34 @@ export default class Recorder {
   }
 
   async getLatestRecord(serviceId, documentType) {
-    const filePath = this.getPathFor(serviceId, documentType);
+    const filePath = this.getPathFor(serviceId, documentType, '*');
     const [ latestCommit ] = await this._getCommits(filePath);
 
+    if (!latestCommit) {
+      return {
+        id: null,
+        path: null,
+      };
+    }
+
+    const [ relativeFilePath, ...otherFilesPaths ] = await this.git.filesInCommit(latestCommit.hash);
+
+    if (otherFilesPaths.length) {
+      throw new Error(`The commit ${latestCommit.hash} should only contain one file but contains following files: ${otherFilesPaths}`);
+    }
+
     return {
-      id: latestCommit && latestCommit.hash,
-      path: filePath
+      id: latestCommit.hash,
+      path: `${this.path}/${relativeFilePath}`,
     };
   }
 
-  getPathFor(serviceId, documentType) {
-    return `${this.path}/${serviceId}/${documentType}.${this.fileExtension}`;
+  getPathFor(serviceId, documentType, fileExtension) {
+    return `${this.path}/${serviceId}/${documentType}.${fileExtension || this.fileExtension}`;
   }
 
-  async isTracked(serviceId, documentType) {
-    const filePath = this.getPathFor(serviceId, documentType);
+  async isTracked(serviceId, documentType, fileExtension) {
+    const filePath = this.getPathFor(serviceId, documentType, fileExtension);
     return this.git.isTracked(filePath);
   }
 
