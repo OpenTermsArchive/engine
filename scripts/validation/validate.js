2641e84d0178d582ecbf5b5ac9fb7520264bd798
Nicolas Dupont <npg.dupont@gmail.com>
2020-09-07 15:31:37 +0200

    Fix validate command

--- scripts/validation/validate.js
+++ scripts/validation/validate.js
@@ -48,18 +48,15 @@ let serviceDeclarations;
               describe(type, () => {
                 let content;
                 let filteredContent;
-                let isPDF;
+                let mimeType;
 
                 it('has fetchable URL', async function () {
                   this.timeout(30000);
 
                   const { fetch: location } = service.documents[type];
-                  content = await fetch(location);
-
-                  if (content.constructor.name == 'Blob' && content.type == 'application/pdf') {
-                    isPDF = true;
-                    content = Buffer.from(await content.arrayBuffer());
-                  }
+                  const { content: tmpContent, mimeType: tmpMimeType } = await fetch(location);
+                  content = tmpContent;
+                  mimeType = tmpMimeType;
                 });
 
                 it('has a selector that matches an element in the web page', async function () {
@@ -72,7 +69,7 @@ let serviceDeclarations;
                     content,
                     documentDeclaration: service.documents[type],
                     filterFunctions: service.filters,
-                    isPDF
+                    mimeType,
                   });
 
                   expect(filteredContent).to.not.be.empty;
@@ -107,17 +104,13 @@ let serviceDeclarations;
                     this.timeout(30000);
 
                     const { fetch: location } = service.documents[type];
-                    let secondContent = await fetch(location);
-
-                    if (secondContent.constructor.name == 'Blob' && secondContent.type == 'application/pdf') {
-                      secondContent = Buffer.from(await secondContent.arrayBuffer());
-                    }
+                    const { content: secondContent, mimeType: secondMimetype} = await fetch(location);
 
                     const secondFilteredContent = await filter({
                       content: secondContent,
                       documentDeclaration: service.documents[type],
                       filterFunctions: service.filters,
-                      isPDF
+                      mimeType: secondMimetype
                     });
 
                     expect(secondFilteredContent).to.equal(filteredContent);
