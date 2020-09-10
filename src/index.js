4ba440e7a42f5d50caa7683cb66c122695beb5fb
Nicolas Dupont <npg.dupont@gmail.com>
2020-09-10 15:42:07 +0200

    Making the servicesIds parameter mandatory

--- src/index.js
+++ src/index.js
@@ -7,25 +7,28 @@ import Notifier from './notifier/index.js';
 const args = process.argv.slice(2);
 const refilterOnly = args.includes('--refilter-only');
 const schedule = args.includes('--schedule');
-const services = args.filter(arg => !arg.startsWith('--'));
+let serviceIds = args.filter(arg => !arg.startsWith('--'));
 
 (async () => {
   const app = new CGUs();
   app.attach(logger);
   await app.init();
 
-  logger.info('Refiltering documents… (it could take a while)');
-  await app.refilterAndRecord(services);
-  logger.info('Refiltering done.\n');
+  serviceIds = serviceIds.length ? serviceIds : app.serviceIds;
+  const numberOfDocuments = serviceIds.reduce((acc, serviceId) => acc + Object.keys(app.serviceDeclarations[serviceId].documents).length, 0);
+
+  logger.info(`Refiltering ${numberOfDocuments} documents from ${serviceIds.length} services…`);
+  await app.refilterAndRecord(serviceIds);
+  logger.info(`Refiltered ${numberOfDocuments} documents from ${serviceIds.length} services.\n`);
 
   if (refilterOnly) {
     return;
   }
 
   if (!schedule) {
-    logger.info('Start tracking changes…');
-    await app.trackChanges(services);
-    return logger.info('Tracking changes done.');
+    logger.info(`Start tracking changes of ${numberOfDocuments} documents from ${serviceIds.length} services…`);
+    await app.trackChanges(serviceIds);
+    return logger.info(`Tracked changes of ${numberOfDocuments} documents from ${serviceIds.length} services.`);
   }
 
   const rule = new scheduler.RecurrenceRule();
@@ -34,10 +37,10 @@ const services = args.filter(arg => !arg.startsWith('--'));
   app.attach(new Notifier(app.serviceDeclarations));
 
   logger.info('The scheduler is running…');
-  logger.info('Documents will be tracked at minute 30 past every hour.');
+  logger.info(`Documents will be tracked at minute ${rule.minute} past every hour.`);
   scheduler.scheduleJob(rule, async () => {
-    logger.info('Start tracking changes…');
-    await app.trackChanges(services);
-    logger.info('Tracking changes done.');
+    logger.info(`Start tracking changes of ${numberOfDocuments} documents from ${serviceIds.length} services…`);
+    await app.trackChanges(serviceIds);
+    logger.info(`Tracked changes of ${numberOfDocuments} documents from ${serviceIds.length} services.`);
   });
 })();
