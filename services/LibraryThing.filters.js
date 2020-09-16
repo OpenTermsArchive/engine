26b7a97234daecc21a7a79fc930e5e068e7870b8
Matti Schneider <matti.schneider@diplomatie.gouv.fr>
2020-09-16 18:45:34 +0200

    Update LibraryThing
    Remove “Failed to execute 'setEndBefore' on 'Range': parameter 1 is not of type 'Node'.” errors

--- services/LibraryThing.filters.js
+++ services/LibraryThing.filters.js
@@ -1,33 +1,13 @@
-export function selectOnlyTOS(document) {
-  const sectionTitles = Array.from(document.querySelectorAll('h2'));
-  const privacyPolicyTitle = sectionTitles.filter(el => el.textContent === 'Privacy Policy')[0].firstChild;
-  const TermsOfServiceTitle = sectionTitles.filter(el => el.textContent === 'Terms of Service')[0].firstChild;
-  const mobilePrivacyPolicyTitle = Array.from(document.querySelectorAll('h3')).filter(title => title.textContent === 'Mobile Apps (Android and iOS)')[0];
+export function moveAppPrivacyPolicyIntoGlobalPrivacyPolicy(document, declaration) {
+  const startOfAppPrivacyPolicy = Array.from(document.querySelectorAll('h3')).filter(title => title.textContent === 'Mobile Apps (Android and iOS)')[0];
+  const endOfToS = document.querySelector('[name=coppa] ~ h2');
 
-  const privacyPolicyContent = document.createRange();
-  privacyPolicyContent.setStartBefore(privacyPolicyTitle);
-  privacyPolicyContent.setEndBefore(TermsOfServiceTitle);
-  privacyPolicyContent.deleteContents();
+  const appPrivacyPolicy = document.createRange();
+  appPrivacyPolicy.setStartBefore(startOfAppPrivacyPolicy);
+  appPrivacyPolicy.setEndBefore(endOfToS);
 
-  const mobilePrivacyPolicyContent = document.createRange();
-  mobilePrivacyPolicyContent.setStartBefore(mobilePrivacyPolicyTitle);
-  mobilePrivacyPolicyContent.setEndBefore(document.querySelector('.content > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > *:last-child'));
-  mobilePrivacyPolicyContent.deleteContents();
-}
+  const endOfPrivacyPolicy = document.querySelector('[name="#privacy"] ~ h2 ~ h2');
+  document.querySelector('#lt2_content_interior').insertBefore(appPrivacyPolicy.cloneContents(), endOfPrivacyPolicy);
 
-export function selectOnlyPrivacyPolicy(document) {
-  const sectionTitles = Array.from(document.querySelectorAll('h2'));
-  const communityRulesTitle = sectionTitles.filter(el => el.textContent === 'Community Rules')[0].firstChild;
-  const mobilePrivacyPolicyTitle = Array.from(document.querySelectorAll('h3')).filter(title => title.textContent === 'Mobile Apps (Android and iOS)')[0];
-  const lastTitle = sectionTitles.filter(el => el.textContent === 'Tired, yet?\n\n')[0].firstChild;
-
-  const communityRulesContent = document.createRange();
-  communityRulesContent.setStartBefore(communityRulesTitle);
-  communityRulesContent.setEndBefore(mobilePrivacyPolicyTitle);
-  communityRulesContent.deleteContents();
-
-  const otherContent = document.createRange();
-  otherContent.setStartBefore(lastTitle);
-  otherContent.setEndBefore(document.querySelector('.content > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > *:last-child'));
-  otherContent.deleteContents();
+  appPrivacyPolicy.deleteContents();
 }
