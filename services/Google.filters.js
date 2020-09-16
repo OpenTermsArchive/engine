42743846cf3461c04874a5f72413970a9e58d7e3
Lucas Verney <lucas.verney@finances.gouv.fr>
2020-09-16 17:21:27 +0200

    Filter out country version blinking in Google Terms of Services

--- services/Google.filters.js
+++ services/Google.filters.js
@@ -4,3 +4,19 @@ export function removeUTMfromUrls(document) {
     link.href = link.href.replace(/utm_[^=]*=[^&]*[&]{0,1}/g, '');
   });
 }
+
+export function removeCountryVersion(document) {
+  // Remove "country version" link at top of the page
+  document.querySelector('a[data-name="country-version"]').parentNode.remove();
+
+  // Remove all the "country version" section in the CGUs
+  const countryVersionTitle = document.querySelector('#footnote-country-version');
+  const disclaimerTitle = document.querySelector('#footnote-disclaimer');
+
+  let node = countryVersionTitle;
+  while (node !== disclaimerTitle.previousSibling) {
+    const next = node.nextSibling;
+    node.remove();
+    node = next;
+  }
+}
