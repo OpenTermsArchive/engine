GatienH/OpenTermsArchive

Consider InaccessibleContent only as a warning

src/logger/index.js
@@ -71,12 +71,8 @@ export function onRecordsPublished() {
   logger.info({ message: 'Records published.' });
 }
 
-export function onRefilteringError(serviceId, type, error) {
-  logger.error({ message: `Could not refilter document: ${error.stack}`, serviceId, type });
-}
-
-export function onInaccessibleContentError(serviceId, type, error) {
-  logger.error({ message: `Could not track document: ${error.stack}`, serviceId, type });
+export function onInaccessibleContent(serviceId, type, error) {
+  logger.warn({ message: `Content inacessible: ${error.message}`, serviceId, type });
 }
 
 export function info(options) {

OpenTermsArchive/engine

src/logger/index.js
@@ -71,8 +71,8 @@ export function onRecordsPublished() {
   logger.info({ message: 'Records published.' });
 }
 
-export function onInaccessibleContentError(serviceId, type, error) {
-  logger.error({ message: `Could not track document: ${error.stack}`, serviceId, type });
+export function onInaccessibleContent(serviceId, type, error) {
+  logger.warn({ message: `Content inacessible: ${error.stack}`, serviceId, type });
 }
 
 export function info(options) {