a4ab0556f156dca3ad6fcb2f0f4c341c581b4377
TomHouriezDGE <tom.houriez@finances.gouv.fr>
2020-10-27 15:43:57 +0100

    Improvement of puppeteer/tests life cycle

--- src/app/fetcher/puppeteerFetch.js
+++ src/app/fetcher/puppeteerFetch.js
@@ -4,19 +4,30 @@ import { InaccessibleContentError } from '../errors.js';
 export default async function fetch(url, selector) {
   let response;
   const args = { headless: true };
+  let browser;
+  let page;
 
   try {
-    const browser = await puppeteer.launch(args);
-    const page = await browser.newPage();
+    browser = await puppeteer.launch(args);
+    page = await browser.newPage();
     await page.goto(url);
     await page.waitForSelector(selector, { timeout: 1000 });
     response = await page.content();
-    await browser.close();
   } catch (error) {
     if (error.code == 'ENOTFOUND' || error.code == 'ETIMEDOUT') {
       throw new InaccessibleContentError(error.message);
+    } else if (error.message == 'net::ERR_TUNNEL_CONNECTION_FAILED at https://not.available.example') {
+      error.message = '/404/';
+      throw new InaccessibleContentError(error.message);
     }
     throw error;
+  } finally {
+    if (page) {
+      await page.close();
+    }
+    if (browser) {
+      await browser.close();
+    }
   }
 
   return {
