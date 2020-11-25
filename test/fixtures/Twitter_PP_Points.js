4d122a2f8d1d8d5ea6b42d0b938d1180d086266c
Michiel de Jong <michiel@unhosted.org>
2020-11-25 11:04:48 +0100

    Twitter PP fixtures for development of the quote-matcher

---
+++ test/fixtures/Twitter_PP_Points.js
@@ -0,0 +1,111 @@
+export const Points = [
+  {
+    id: '699',
+    quoteText: 'A cookie is a small piece of data that is stored on your computer or mobile device.\n'
+      + 'Like many websites, we use cookies and similar technologies to collect additional website usage data and to operate our services.\n'
+      + 'Cookies are not required for many parts of our services such as searching and looking at public profiles.\n'
+      + 'Although most web browsers automatically accept cookies, many browsers’ settings can be set to decline cookies or alert you when a website is attempting to place a cookie on your computer.\n'
+      + 'However, some of our services may not function properly if you disable cookies.\n'
+      + 'When your browser or device allows it, we use both session cookies and persistent cookies to better understand how you interact with our services, to monitor aggregate usage patterns, and to personalize and otherwise operate our services such as by providing account security, personalizing the content we show you including ads, and remembering your language preferences.\n'
+      + 'We do not support the Do Not Track browser option.\n'
+      + 'You can learn more about how we use cookies and similar technologies here.</p>\n'
+      + '<p>pageReference: /content/twitter-com/legal/en/privacy/chapter-2/chapter-2-3-tooltips---overlays</p>\n'
+      + '<p>Content:</p>\n'
+      + '<p>Not hungry?</p>\n'
+      + '<p>You can change your cookie settings in your web browser.</p>',
+    quoteStart: 16070,
+    quoteEnd: 17362
+  },
+  {
+    id: '672',
+    quoteText: ' <p>4.2</p> Deletion <p>We keep Log Data for a maximum of 18 months.\n'
+      + 'If you follow the instructions here (or for Periscope here), your account will be deactivated and then deleted.\n'
+      + 'When deactivated, your Twitter account, including your display name, username, and public profile, will no longer be viewable on Twitter.com, Twitter for iOS, and Twitter for Android.\n'
+      + 'For up to 30 days after deactivation it is still possible to restore your Twitter account if it was accidentally or wrongfully deactivated.</p>\n'
+      + '<p>Keep in mind that search engines and other third parties may still retain copies of your public information, like your profile information and public Tweets, even after you have deleted the information from our services or deactivated your account.\n'
+      + 'Learn more here.</p>',
+    quoteStart: 33188,
+    quoteEnd: 33969
+  },
+  {
+    id: '756',
+    quoteText: 'You can download certain account information, including your Tweets, by following the instructions here.\n'
+      + 'On Periscope, you can request correction, deletion, or modification of your personal data, and download your account information, by following the instructions here.',
+    quoteStart: 32774,
+    quoteEnd: 33044
+  },
+  {
+    id: '779',
+    quoteText: 'They may also collect other personal data from you, such as cookie identifiers or your IP address.</p>',
+    quoteStart: 15943,
+    quoteEnd: 16045
+  },
+  {
+    id: '5616',
+    quoteText: 'Our services are not directed to children, and you may not use our services if you are under the age of 13.\n'
+      + 'You must also be old enough to consent to the processing of your personal data in your country (in some countries we may allow your parent or guardian to do so on your behalf).\n'
+      + 'You must be at least 16 years of age to use Periscope.</p>',
+    quoteStart: 35656,
+    quoteEnd: 35999
+  },
+  {
+    id: '655',
+    quoteText: '<p>3.2</p> Service Providers <p>We engage service providers to perform functions and provide services to us in the United States, Ireland, and other countries.\n'
+      + 'For example, we use a variety of third-party services to help operate our services, such as hosting our various blogs and wikis, and to help us understand the use of our services, such as Google Analytics.\n'
+      + 'We may share your private personal data with such service providers subject to obligations consistent with this Privacy Policy and any other appropriate confidentiality and security measures, and on the condition that the third parties use your private personal data only on our behalf and pursuant to our instructions.\n'
+      + 'We share your payment information with payment services providers to process payments.\n'
+      + 'prevent, detect, and investigate fraud or other prohibited activities.\n'
+      + 'facilitate dispute resolution such as chargebacks or refunds.\n'
+      + 'and for other purposes associated with the acceptance of credit and debit cards.',
+    quoteStart: 28466,
+    quoteEnd: 29452
+  },
+  {
+    id: '730',
+    quoteText: 'When you view our content on third-party websites that integrate Twitter content such as embedded timelines or Tweet buttons, we may receive Log Data that includes the web page you visited.\n'
+      + 'We use this information to better understand the use of our services, to protect the safety and integrity of our platform, and to show more relevant content, including ads.\n'
+      + 'We do not associate this web browsing history with your name, email address, phone number, or username, and we delete, obfuscate, or aggregate it after no longer than 30 days.\n'
+      + 'We do not collect this data from browsers that we believe to be located in the European Union or EFTA States.<br>',
+    quoteStart: 19224,
+    quoteEnd: 19876
+  },
+  {
+    id: '614',
+    quoteText: 'Changes to This Privacy Policy   <p>We may revise this Privacy Policy from time to time.\n'
+      + 'The most current version of the policy will govern our processing of your personal data and will always be at https://twitter.com/privacy.\n'
+      + 'If we make a change to this policy that, in our sole discretion, is material, we will notify you via an @Twitter update or email to the email address associated with your account.\n'
+      + 'By continuing to access or use the Services after those changes become effective, you agree to be bound by the revised Privacy Policy.</p>',
+    quoteStart: 38105,
+    quoteEnd: 38651
+  },
+  {
+    id: '5847',
+    quoteText: '3.2</p> Service Providers <p>We engage service providers to perform functions and provide services to us in the United States, Ireland, and other countries.\n'
+      + 'For example, we use a variety of third-party services to help operate our services, such as hosting our various blogs and wikis, and to help us understand the use of our services, such as Google Analytics.\n'
+      + 'We may share your private personal data with such service providers subject to obligations consistent with this Privacy Policy and any other appropriate confidentiality and security measures, and on the condition that the third parties use your private personal data only on our behalf and pursuant to our instructions.\n'
+      + 'We share your payment information with payment services providers to process payments.\n'
+      + 'prevent, detect, and investigate fraud or other prohibited activities.\n'
+      + 'facilitate dispute resolution such as chargebacks or refunds.\n'
+      + 'and for other purposes associated with the acceptance of credit and debit cards.</p>',
+    quoteStart: 28469,
+    quoteEnd: 29456
+  },
+  {
+    id: '5853',
+    quoteText: 'We use this information for things like keeping your account secure and showing you more relevant Tweets, people to follow, events, and ads.',
+    quoteStart: 1453,
+    quoteEnd: 1593
+  },
+  {
+    id: '5852',
+    quoteText: '3.4</p> Affiliates and Change of Ownership <p>In the event that we are involved in a bankruptcy, merger, acquisition, reorganization, or sale of assets, your personal data may be sold or transferred as part of that transaction.\n',
+    quoteStart: 30686,
+    quoteEnd: 30914
+  },
+  {
+    id: '5849',
+    quoteText: 'You can also always download the information you have shared on Twitter.',
+    quoteStart: 1857,
+    quoteEnd: 1929
+  }
+];
