d8514a795eb2d4e1dad70a5e7a585dd49e4caa28
Nicolas Dupont <npg.dupont@gmail.com>
2020-11-26 15:30:20 +0100

    Add executeClientScripts option to DocumentDeclaration class

--- src/app/services/documentDeclaration.js
+++ src/app/services/documentDeclaration.js
@@ -1,8 +1,9 @@
 export default class DocumentDeclaration {
-  constructor({ service, type, location, contentSelectors, noiseSelectors, filters, validUntil = null }) {
+  constructor({ service, type, location, executeClientScripts, contentSelectors, noiseSelectors, filters, validUntil = null }) {
     this.service = service;
     this.type = type;
     this.location = location;
+    this.executeClientScripts = executeClientScripts;
     this.contentSelectors = contentSelectors;
     this.noiseSelectors = noiseSelectors;
     this.filters = filters;
