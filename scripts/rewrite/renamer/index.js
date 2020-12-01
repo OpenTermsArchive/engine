464e5d2ff3b648ec3949f34215e443cefd24c636
Nicolas Dupont <npg.dupont@gmail.com>
2020-12-01 09:58:49 +0100

    Refactor rewrite snapshots script

---
+++ scripts/rewrite/renamer/index.js
@@ -0,0 +1,41 @@
+import fsApi from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const fs = fsApi.promises;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+let renamingRules;
+
+export async function loadRules() {
+  renamingRules = {
+    services: JSON.parse(await fs.readFile(path.resolve(__dirname, './rules/services.json'))),
+    documentTypes: JSON.parse(await fs.readFile(path.resolve(__dirname, './rules/documentTypes.json'))),
+    servicesDocumentTypes: JSON.parse(await fs.readFile(path.resolve(__dirname, './rules/servicesDocumentTypes.json'))),
+  };
+}
+
+export function applyRules(serviceId, documentType) {
+  const renamedServiceId = renamingRules.services[serviceId];
+  if (renamedServiceId) {
+    console.log(`⌙ Rename service "${serviceId}" to "${renamedServiceId}"`);
+    serviceId = renamedServiceId;
+  }
+
+  const renamedDocumentType = renamingRules.documentTypes[documentType];
+  if (renamedDocumentType) {
+    console.log(`⌙ Rename document type "${documentType}" to "${renamedDocumentType}" of "${serviceId}" service`);
+    documentType = renamedDocumentType;
+  }
+
+  const renamedServiceDocumentType = renamingRules.servicesDocumentTypes[serviceId] && renamingRules.servicesDocumentTypes[serviceId][documentType];
+  if (renamedServiceDocumentType) {
+    console.log(`⌙ Specific rename document type "${documentType}" to "${renamedServiceDocumentType}" of "${serviceId}" service`);
+    documentType = renamedServiceDocumentType;
+  }
+
+  return {
+    renamedServiceId: serviceId,
+    renamedDocumentType: documentType,
+  };
+}
--- scripts/rewrite/rewrite-snapshots.js
+++ scripts/rewrite/rewrite-snapshots.js
@@ -1,19 +1,16 @@
-import fsApi from 'fs';
 import path from 'path';
 import { pathToFileURL, fileURLToPath } from 'url';
 
 import config from 'config';
-import mime from 'mime';
 
 import Git from '../../src/app/history/git.js';
+import { loadFile } from './utils.js';
+import * as renamer from './renamer/index.js';
+import * as initier from './initializer/index.js';
 
-const fs = fsApi.promises;
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export const SNAPSHOTS_SOURCE_PATH = path.resolve(__dirname, '../../', config.get('rewrite.snapshotsSourcePath'));
 export const SNAPSHOTS_TARGET_PATH = path.resolve(__dirname, '../../', config.get('history.snapshotsPath'));
-
-const README_PATH = path.resolve(__dirname, './initFiles/snapshot-readme.md');
-const LICENSE_PATH = path.resolve(__dirname, './initFiles/snapshot-license');
 
 const EMPTY_TOS_BACK_CONTENT = `<!DOCTYPE html><html><head></head><body>
 
@@ -25,7 +22,7 @@ let history;
   console.log('Start rewritting history.');
   const init = process.argv.includes('--init');
 
-  const renamingRules = await loadRenamingRules();
+  const renamingRules = await renamer.loadRules();
 
   const sourceRepo = new Git(SNAPSHOTS_SOURCE_PATH);
 
@@ -33,10 +30,10 @@ let history;
   const commits = (await sourceRepo.log([ '--stat=4096' ])).sort((a, b) => new Date(a.date) - new Date(b.date));
 
   if (init) {
-    const targetRepo = await initTargetRepo(SNAPSHOTS_TARGET_PATH);
+    const targetRepo = await initier.initTargetRepo(SNAPSHOTS_TARGET_PATH);
 
     const [ readmeCommit ] = commits;
-    await initReadmeAndLicense(targetRepo, readmeCommit.date);
+    await initier.initReadmeAndLicense(targetRepo, SNAPSHOTS_TARGET_PATH, readmeCommit.date);
   }
 
   history = await import(pathToFileURL(path.resolve(__dirname, '../..', 'src/app/history/index.js'))); // history module needs the target repo to be initiliazed. So loads it after target repo initialization.
@@ -70,7 +67,7 @@ let history;
 
     const [{ file: relativeFilePath }] = commit.diff.files;
 
-    const { content, mimeType } = await loadFile(relativeFilePath);
+    const { content, mimeType } = await loadFile(SNAPSHOTS_SOURCE_PATH, relativeFilePath);
 
     let serviceId = path.dirname(relativeFilePath);
     let documentType = path.basename(relativeFilePath, path.extname(relativeFilePath));
@@ -81,7 +78,7 @@ let history;
       continue;
     }
 
-    const { renamedServiceId, renamedDocumentType } = applyRenamingRules(serviceId, documentType, renamingRules);
+    const { renamedServiceId, renamedDocumentType } = renamer.applyRules(serviceId, documentType, renamingRules);
     serviceId = renamedServiceId;
     documentType = renamedDocumentType;
 
@@ -136,85 +133,3 @@ let history;
     console.error('\n⚠ WARNING: Total treated commits does not match the total number of commits to be treated! ⚠');
   }
 }());
-
-async function initReadmeAndLicense(targetRepo, authorDate) {
-  const targetReadmeFilePath = path.resolve(SNAPSHOTS_TARGET_PATH, 'README.md');
-  const targetLicenseFilePath = path.resolve(SNAPSHOTS_TARGET_PATH, 'LICENSE');
-  await fs.copyFile(README_PATH, targetReadmeFilePath);
-  await fs.copyFile(LICENSE_PATH, targetLicenseFilePath);
-  await targetRepo.add(targetReadmeFilePath);
-  await targetRepo.add(targetLicenseFilePath);
-  await targetRepo.commit(null, 'Add Readme and License', authorDate);
-}
-
-async function initTargetRepo(targetRepoPath) {
-  console.log('Initializing target repository');
-
-  if (await fileExists(targetRepoPath)) {
-    console.log(`Removed already existing target repository: ${targetRepoPath}`);
-    await fs.rmdir(targetRepoPath, { recursive: true });
-  }
-  await fs.mkdir(targetRepoPath);
-
-  await new Git(targetRepoPath).init();
-  await new Git(targetRepoPath).initConfig();
-  return new Git(targetRepoPath); // Re-intanciation of the target repo after initialization is required to ensure configuration made in the Git class is properly done.
-}
-
-async function loadRenamingRules() {
-  return {
-    services: JSON.parse(await fs.readFile(path.resolve(__dirname, './renamingRules/services.json'))),
-    documentTypes: JSON.parse(await fs.readFile(path.resolve(__dirname, './renamingRules/documentTypes.json'))),
-    servicesDocumentTypes: JSON.parse(await fs.readFile(path.resolve(__dirname, './renamingRules/servicesDocumentTypes.json'))),
-  };
-}
-
-async function loadFile(relativeFilePath) {
-  const absoluteFilePath = `${SNAPSHOTS_SOURCE_PATH}/${relativeFilePath}`;
-
-  const mimeType = mime.getType(absoluteFilePath);
-  const readFileOptions = {};
-  if (mimeType.startsWith('text/')) {
-    readFileOptions.encoding = 'utf8';
-  }
-  return {
-    content: await fs.readFile(absoluteFilePath, readFileOptions),
-    mimeType,
-  };
-}
-
-function applyRenamingRules(serviceId, documentType, renamingRules) {
-  const renamedServiceId = renamingRules.services[serviceId];
-  if (renamedServiceId) {
-    console.log(`⌙ Rename service "${serviceId}" to "${renamedServiceId}"`);
-    serviceId = renamedServiceId;
-  }
-
-  const renamedDocumentType = renamingRules.documentTypes[documentType];
-  if (renamedDocumentType) {
-    console.log(`⌙ Rename document type "${documentType}" to "${renamedDocumentType}" of "${serviceId}" service`);
-    documentType = renamedDocumentType;
-  }
-
-  const renamedServiceDocumentType = renamingRules.servicesDocumentTypes[serviceId] && renamingRules.servicesDocumentTypes[serviceId][documentType];
-  if (renamedServiceDocumentType) {
-    console.log(`⌙ Specific rename document type "${documentType}" to "${renamedServiceDocumentType}" of "${serviceId}" service`);
-    documentType = renamedServiceDocumentType;
-  }
-
-  return {
-    renamedServiceId: serviceId,
-    renamedDocumentType: documentType,
-  };
-}
-
-async function fileExists(filePath) {
-  try {
-    await fs.access(filePath);
-    return true;
-  } catch (error) {
-    if (error.code === 'ENOENT') {
-      return false;
-    }
-  }
-}
