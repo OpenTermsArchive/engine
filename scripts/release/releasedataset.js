cfa91f478654c12c3add20d28d3882b2214a6c73
Martin Ratinaud <martinratinaud@gmail.com>
2021-03-30 12:19:49 +0400

    feat: add scheduler to create release every night

--- scripts/release/releasedataset.js
+++ scripts/release/releasedataset.js
@@ -1,11 +1,10 @@
 import config from 'config';
 import { execSync } from 'child_process';
 
-async function main() {
-  const result = execSync(`sh ./scripts/release/build_release.sh ${config.get('history.versionsPath')}`, { stdio: 'inherit' });
+export async function publishRelease() {
+  const result = execSync(
+    `sh ./scripts/release/build_release.sh ${config.get('history.versionsPath')}`,
+    { stdio: 'inherit' }
+  );
   return result;
 }
-
-(async () => {
-  await main();
-})();
