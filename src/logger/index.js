72263755a2d98e617090ebcaa72c02c4fa09a967
Martin Ratinaud <martinratinaud@gmail.com>
2021-04-01 09:55:06 +0400

    fix: do not exit on error when sending email as it makes the docker container stop

--- src/logger/index.js
+++ src/logger/index.js
@@ -33,6 +33,7 @@ if (config.get('logger.sendMailOnError')) {
     ssl: true,
     timeout: 30 * 1000,
     formatter: (args) => args[Object.getOwnPropertySymbols(args)[1]], // Returns the full error message, the same visible in the console. It is referenced in the argument object with a Symbol of which we do not have the reference but we know it is the second one.
+    exitOnError: false,
   };
 
   transports.push(
