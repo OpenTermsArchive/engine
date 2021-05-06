6e18a9ccabfed75e8ee6cd883a1b2b0861442071
Martin Ratinaud <martinratinaud@gmail.com>
2021-05-06 22:50:21 +0400

    Revert "wip: create the release on every deploy"
    This reverts commit fae27f425bbf32dcf766fee628c3ae45de386a61.

--- src/index.js
+++ src/index.js
@@ -65,10 +65,6 @@ const schedule = args.includes('--schedule');
     `Tracked changes of ${numberOfDocuments} documents from ${serviceIds.length} services.`
   );
 
-  logger.info(`Start Release ${new Date()}`);
-  await publishRelease();
-  logger.info(`End Release ${new Date()}`);
-
   if (!schedule) {
     return;
   }
