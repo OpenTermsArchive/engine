f7a0f32db278252ec074d444c9eb99eae4aeae35
Martin Ratinaud <martinratinaud@gmail.com>
2021-08-24 10:31:53 +0400

    chore: fix not passing tests because empty text commits are not allowed anymore

--- src/app/history/recorder.js
+++ src/app/history/recorder.js
@@ -49,7 +49,7 @@ export default class Recorder {
   async commit(filePath, message, authorDate) {
     try {
       await this.git.add(filePath);
-      return await this.git.commit(filePath, message, authorDate);
+      return this.git.commit(filePath, message, authorDate);
     } catch (error) {
       throw new Error(
         `Could not commit ${filePath} with message "${message}" due to error: "${error}"`
