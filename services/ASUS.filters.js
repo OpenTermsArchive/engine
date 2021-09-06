3a8a4ddf6a9fd4e624b34f22459430eceb92e0dc
Clément Biron <clement.biron@gmail.com>
2021-09-06 16:27:40 +0200

    fix: update ASUS Vulnerability Disclosure Policy

---
+++ services/ASUS.filters.js
@@ -0,0 +1,20 @@
+export function removeSecurityUpdates(document) {
+  const aTags = document.getElementsByTagName('h3');
+  const searchText = 'Latest security updates';
+  let found;
+
+  for (let i = 0; i < aTags.length; i++) {
+    if (aTags[i].textContent == searchText) {
+      found = aTags[i];
+      break;
+    }
+  }
+
+  let nextEl = found.nextSibling;
+  while (nextEl != null) {
+    found.parentNode.removeChild(nextEl);
+    nextEl = found.nextSibling;
+  }
+
+  found.remove();
+}
