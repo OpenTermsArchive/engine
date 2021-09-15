1d1edb917570801af793c2895f345e1cb24e7554
Martin Ratinaud <martinratinaud@gmail.com>
2021-09-15 16:34:19 +0400

    feat: attempt retry through proxy when puppeeteer fails

--- src/app/fetcher/fullDomFetcher.js
+++ src/app/fetcher/fullDomFetcher.js
@@ -2,16 +2,17 @@ import { InaccessibleContentError } from '../errors.js';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import config from 'config';
 import { getRandomProxy } from './proxy.js';
+import logger from '../../logger/index.js';
 import puppeteer from 'puppeteer-extra';
 import useProxy from 'puppeteer-page-proxy';
 
 puppeteer.use(StealthPlugin());
 
 const PUPPETEER_TIMEOUT = 60 * 1000; // 60s
-
+const MAX_RETRIES = 3;
 let browser;
 
-export default async function fetch(url, cssSelectors, headers = {}) {
+export default async function fetch(url, cssSelectors, headers = {}, { retry } = { retry: 0 }) {
   let response;
   let content;
   let page;
@@ -26,13 +27,30 @@ export default async function fetch(url, cssSelectors, headers = {}) {
     }
 
     page = await browser.newPage();
-    await useProxy(page, await getRandomProxy());
+
+    if (retry > 0 && process.env.NODE_ENV !== 'test') {
+      try {
+        const randomProxy = await getRandomProxy();
+        await useProxy(page, randomProxy);
+        logger.info(`Retry ${retry} with proxy ${randomProxy} for url ${url}`);
+      } catch (e) {
+        logger.error('Could not use proxy');
+      }
+    }
     await page.setDefaultNavigationTimeout(PUPPETEER_TIMEOUT);
 
-    response = await page.goto(url, { waitUntil: 'networkidle0' });
     await page.setExtraHTTPHeaders({
       ...headers,
     });
+
+    response = await page.goto(url, { waitUntil: 'networkidle0' });
+
+    if (!response) {
+      if (retry === MAX_RETRIES) {
+        throw new InaccessibleContentError(`Response is empty when trying to fetch '${url}'`);
+      }
+      return await fetch(url, cssSelectors, headers, { retry: retry + 1 });
+    }
 
     const statusCode = response.status();
 
@@ -50,10 +68,16 @@ export default async function fetch(url, cssSelectors, headers = {}) {
 
     content = await page.content();
   } catch (error) {
+    if (retry < MAX_RETRIES && process.env.NODE_ENV !== 'test') {
+      logger.warn(`Error ${error.message} on url ${url}, retrying again ${retry + 1} times`);
+
+      return await fetch(url, cssSelectors, headers, { retry: retry + 1 });
+    }
+
     if (
       (error.code && error.code.match(/^(EAI_AGAIN|ENOTFOUND|ETIMEDOUT|ECONNRESET)$/)) ||
       (error.message &&
-        error.message.match(/(ERR_TUNNEL_CONNECTION_FAILED|ERR_NAME_NOT_RESOLVED)/)) ||
+        error.message.match(/(ERR_FAILED|ERR_TUNNEL_CONNECTION_FAILED|ERR_NAME_NOT_RESOLVED)/)) ||
       error instanceof puppeteer.pptr.errors.TimeoutError // Expected elements are not present on the web page
     ) {
       throw new InaccessibleContentError(error.message);
