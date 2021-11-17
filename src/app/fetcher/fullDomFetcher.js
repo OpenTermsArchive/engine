6e35d52d43fe808758fa33d6c656594c614c4992
Martin Ratinaud <martinratinaud@gmail.com>
2021-11-17 17:06:57 +0400

    Remove retry mechanism as it is not used anymore

--- src/app/fetcher/fullDomFetcher.js
+++ src/app/fetcher/fullDomFetcher.js
@@ -8,10 +8,10 @@ import { FetchDocumentError } from './errors.js';
 puppeteer.use(StealthPlugin());
 
 const PUPPETEER_TIMEOUT = 30 * 1000; // 30 seconds in ms
-const MAX_RETRIES = 3;
+
 let browser;
 
-export default async function fetch(url, cssSelectors, { retry } = { retry: 0 }) {
+export default async function fetch(url, cssSelectors) {
   let page;
   let response;
   let content;
@@ -29,11 +29,7 @@ export default async function fetch(url, cssSelectors, { retry } = { retry: 0 })
     response = await page.goto(url, { waitUntil: 'networkidle0' });
 
     if (!response) {
-      if (retry === MAX_RETRIES) {
-        throw new FetchDocumentError(`Response is empty when trying to fetch '${url}'`);
-      }
-
-      return await fetch(url, cssSelectors, { retry: retry + 1 });
+      throw new FetchDocumentError(`Response is empty when trying to fetch '${url}'`);
     }
 
     const statusCode = response.status();
