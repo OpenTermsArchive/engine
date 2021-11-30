3f04e9996dfb70fe91911246181545fd74ef295c
Nicolas Dupont <npg.dupont@gmail.com>
2021-11-30 09:40:21 +0100

    Add comment

--- bin/validate.js
+++ bin/validate.js
@@ -10,9 +10,9 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const VALIDATE_PATH = path.resolve(__dirname, '../scripts/validation/validate.js');
 
 (async () => {
-  mocha.delay();
-  mocha.addFile(VALIDATE_PATH);
-  await mocha.loadFilesAsync();
+  mocha.delay(); // As the validation script performs an asynchronous load before running the tests, the execution of the tests must be delayed. It works in addition to the `run` instruction after the loading has been done in the validation script.
+  mocha.addFile(VALIDATE_PATH); // As `delay` has been called, this statement will not load the file directly, `loadFilesAsync` is required.
+  await mocha.loadFilesAsync(); // Load files previously added to the Mocha cache with `addFile`.
 
   let hasFailedTests = false;
 
