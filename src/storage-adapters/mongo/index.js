45dda00fdbc18b91215d8d63820691ffd037ad2d
Nicolas Dupont <npg.dupont@gmail.com>
2021-12-02 10:21:22 +0100

    Create Mongo storage adapter

---
+++ src/storage-adapters/mongo/index.js
@@ -0,0 +1,91 @@
+import { MongoClient, ObjectId } from 'mongodb';
+
+const PDF_MIME_TYPE = 'application/pdf';
+
+export default class MongoAdapter {
+  constructor({ database: databaseName, collection: collectionName, connectionURI }) {
+    const client = new MongoClient(connectionURI);
+
+    this.databaseName = databaseName;
+    this.collectionName = collectionName;
+    this.client = client;
+  }
+
+  async initialize() {
+    await this.client.connect();
+    const db = this.client.db(this.databaseName);
+
+    this.collection = db.collection(this.collectionName);
+  }
+
+  async finalize() {
+    await this.client.close();
+  }
+
+  async record({ serviceId, documentType, content: passedContent, mimeType, fetchDate, isRefilter, snapshotId }) {
+    let content = passedContent;
+
+    if (mimeType == PDF_MIME_TYPE) {
+      content = passedContent.toString('utf-8'); // Serialize PDF
+    }
+
+    const previousRecord = await this.getLatestRecord(serviceId, documentType);
+
+    if (previousRecord.id && previousRecord.content == content) {
+      return {
+        id: null,
+        isFirstRecord: false,
+      };
+    }
+
+    const recordProperties = Object.fromEntries(Object.entries({
+      serviceId,
+      documentType,
+      content: passedContent,
+      mimeType,
+      fetchDate,
+      isRefilter,
+      snapshotId,
+    }).filter(([ , value ]) => value)); // Remove empty values
+
+    const isFirstRecord = !await this.collection.findOne({ serviceId, documentType });
+
+    recordProperties.snapshotId = new ObjectId(snapshotId);
+
+    if (isFirstRecord) {
+      recordProperties.isFirstRecord = isFirstRecord;
+    }
+
+    const insertResult = await this.collection.insertOne({ ...recordProperties, created_at: new Date() });
+
+    return {
+      id: insertResult.insertedId.toString(),
+      isFirstRecord,
+    };
+  }
+
+  async getLatestRecord(serviceId, documentType) {
+    const [record] = await this.collection.find({ serviceId, documentType }).limit(1).sort({ fetchDate: -1 }).toArray();
+
+    if (!record) {
+      return {
+        id: null,
+        content: null,
+        mimeType: null,
+      };
+    }
+
+    const { _id, content, mimeType, fetchDate } = record;
+
+    return {
+      id: _id.toString(),
+      content,
+      mimeType,
+      fetchDate: new Date(fetchDate),
+    };
+  }
+
+  async _removeAllRecords() {
+    return this.collection.deleteMany({});
+  }
+}
---
+++ src/storage-adapters/mongo/index.test.js
@@ -0,0 +1,501 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import chai from 'chai';
+import config from 'config';
+import { MongoClient } from 'mongodb';
+
+import MongoAdapter from './index.js';
+
+const { expect } = chai;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const { connectionURI } = config.get('recorder.snapshots.storage.mongo');
+const client = new MongoClient(connectionURI);
+
+const SERVICE_PROVIDER_ID = 'test_service';
+const DOCUMENT_TYPE = 'Terms of Service';
+const CONTENT = 'ToS fixture data with UTF-8 çhãràčtęrs';
+const MIME_TYPE = 'text/html';
+const FETCH_DATE = new Date('2000-01-01T12:00:00.000Z');
+const FETCH_DATE_LATER = new Date('2000-01-02T12:00:00.000Z');
+const SNAPSHOT_ID = '61af86dc5ff5caa74ae926ad';
+const PDF_CONTENT = fs.readFileSync(path.resolve(__dirname, '../../../test/fixtures/terms.pdf'), { encoding: 'utf8' });
+const UPDATED_PDF_CONTENT = fs.readFileSync(path.resolve(__dirname, '../../../test/fixtures/termsModified.pdf'), { encoding: 'utf8' });
+const PDF_MIME_TYPE = 'application/pdf';
+
+let collection;
+
+describe('MongoAdapter', () => {
+  let subject;
+
+  before(async () => {
+    subject = new MongoAdapter(config.get('recorder.snapshots.storage.mongo'));
+    await subject.initialize();
+    await client.connect();
+    const db = client.db(config.get('recorder.snapshots.storage.mongo').database);
+
+    collection = db.collection(config.get('recorder.snapshots.storage.mongo').collection);
+  });
+
+  describe('#record', () => {
+    let record;
+    let mongoRecord;
+    let numberOfRecordsBefore;
+    let numberOfRecordsAfter;
+
+    context('when it is the first record', () => {
+      before(async () => {
+        numberOfRecordsBefore = await collection.find({
+          serviceId: SERVICE_PROVIDER_ID,
+          documentType: DOCUMENT_TYPE,
+        }).count();
+
+        (record = await subject.record({
+          serviceId: SERVICE_PROVIDER_ID,
+          documentType: DOCUMENT_TYPE,
+          content: CONTENT,
+          mimeType: MIME_TYPE,
+          fetchDate: FETCH_DATE,
+          snapshotId: SNAPSHOT_ID,
+        }));
+
+        numberOfRecordsAfter = await collection.find({
+          serviceId: SERVICE_PROVIDER_ID,
+          documentType: DOCUMENT_TYPE,
+        }).count();
+
+        (mongoRecord = await collection.findOne({
+          serviceId: SERVICE_PROVIDER_ID,
+          documentType: DOCUMENT_TYPE,
+        }));
+      });
+
+      after(() => collection.drop());
+
+      it('saves the record', () => {
+        expect(numberOfRecordsAfter).to.equal(numberOfRecordsBefore + 1);
+      });
+
+      it('returns the record id', () => {
+        expect(mongoRecord._id.toString()).to.equal(record.id);
+      });
+
+      it('returns a boolean to know if it is the first record', () => {
+        expect(record.isFirstRecord).to.be.true;
+      });
+
+      it('stores the service id', () => {
+        expect(mongoRecord.serviceId).to.include(SERVICE_PROVIDER_ID);
+      });
+
+      it('stores the document type', () => {
+        expect(mongoRecord.documentType).to.include(DOCUMENT_TYPE);
+      });
+
+      it('stores information that it is the first record for this specific document', () => {
+        expect(mongoRecord.isFirstRecord).to.be.true;
+      });
+
+      it('stores the proper content', () => {
+        expect(mongoRecord.content).to.equal(CONTENT);
+      });
+
+      context('when provided', () => {
+        it('stores the fetch date', () => {
+          expect(new Date(mongoRecord.fetchDate).getTime()).to.equal(FETCH_DATE.getTime());
+        });
+
+        it('stores the mime type', () => {
+          expect(mongoRecord.mimeType).to.equal(MIME_TYPE);
+        });
+
+        it('stores the snapshot ID', () => {
+          expect(mongoRecord.snapshotId.toString()).to.include(SNAPSHOT_ID);
+        });
+      });
+    });
+
+    context('when it is not the first record', () => {
+      const UPDATED_CONTENT = `${CONTENT} updated`;
+
+      before(async () => {
+        (record = await subject.record({
+          serviceId: SERVICE_PROVIDER_ID,
+          documentType: DOCUMENT_TYPE,
+          content: CONTENT,
+          mimeType: MIME_TYPE,
+          fetchDate: FETCH_DATE,
+          snapshotId: SNAPSHOT_ID,
+        }));
+
+        numberOfRecordsBefore = await collection.find({
+          serviceId: SERVICE_PROVIDER_ID,
+          documentType: DOCUMENT_TYPE,
+        }).count();
+
+        (record = await subject.record({
+          serviceId: SERVICE_PROVIDER_ID,
+          documentType: DOCUMENT_TYPE,
+          content: UPDATED_CONTENT,
+          mimeType: MIME_TYPE,
+          fetchDate: FETCH_DATE,
+          snapshotId: SNAPSHOT_ID,
+        }));
+
+        numberOfRecordsAfter = await collection.find({
+          serviceId: SERVICE_PROVIDER_ID,
+          documentType: DOCUMENT_TYPE,
+        }).count();
+
+        ([mongoRecord] = await collection.find({
+          serviceId: SERVICE_PROVIDER_ID,
+          documentType: DOCUMENT_TYPE,
+        }).limit(1).sort({ created_at: -1 }).toArray());
+      });
+
+      after(() => collection.drop());
+
+      it('saves the record', () => {
+        expect(numberOfRecordsAfter).to.equal(numberOfRecordsBefore + 1);
+      });
+
+      it('returns the record id', () => {
+        expect(mongoRecord._id.toString()).to.equal(record.id);
+      });
+
+      it('returns a boolean to know if it is the first record', () => {
+        expect(record.isFirstRecord).to.be.false;
+      });
+
+      it('stores the service id', () => {
+        expect(mongoRecord.serviceId).to.include(SERVICE_PROVIDER_ID);
+      });
+
+      it('stores the document type', () => {
+        expect(mongoRecord.documentType).to.include(DOCUMENT_TYPE);
+      });
+
+      it('stores information that it is the first record for this specific document', () => {
+        expect(mongoRecord.isFirstRecord).to.be.undefined;
+      });
+
+      it('stores the proper content', () => {
+        expect(mongoRecord.content).to.equal(UPDATED_CONTENT);
+      });
+
+      context('when provided', () => {
+        it('stores the fetch date', () => {
+          expect(new Date(mongoRecord.fetchDate).getTime()).to.equal(FETCH_DATE.getTime());
+        });
+
+        it('stores the mime type', () => {
+          expect(mongoRecord.mimeType).to.equal(MIME_TYPE);
+        });
+
+        it('stores the snapshot ID', () => {
+          expect(mongoRecord.snapshotId.toString()).to.include(SNAPSHOT_ID);
+        });
+      });
+    });
+
+    context('when the content has not changed', () => {
+      before(async () => {
+        await subject.record({
+          serviceId: SERVICE_PROVIDER_ID,
+          documentType: DOCUMENT_TYPE,
+          content: CONTENT,
+          mimeType: MIME_TYPE,
+          fetchDate: FETCH_DATE,
+        });
+
+        numberOfRecordsBefore = await collection.find({
+          serviceId: SERVICE_PROVIDER_ID,
+          documentType: DOCUMENT_TYPE,
+        }).count();
+
+        (record = await subject.record({
+          serviceId: SERVICE_PROVIDER_ID,
+          documentType: DOCUMENT_TYPE,
+          content: CONTENT,
+          mimeType: MIME_TYPE,
+          fetchDate: FETCH_DATE_LATER,
+        }));
+
+        numberOfRecordsAfter = await collection.find({
+          serviceId: SERVICE_PROVIDER_ID,
+          documentType: DOCUMENT_TYPE,
+        }).count();
+      });
+
+      after(() => collection.drop());
+
+      it('does not save the record', async () => {
+        expect(numberOfRecordsAfter).to.equal(numberOfRecordsBefore);
+      });
+
+      it('returns an id null', async () => {
+        expect(record.id).to.equal(null);
+      });
+    });
+
+    context('when it is a refilter', () => {
+      const REFILTERED_CONTENT = `${CONTENT} refiltered`;
+
+      before(async () => {
+        await subject.record({
+          serviceId: SERVICE_PROVIDER_ID,
+          documentType: DOCUMENT_TYPE,
+          content: CONTENT,
+        }); // A refilter cannot be the first record
+
+        numberOfRecordsBefore = await collection.find({
+          serviceId: SERVICE_PROVIDER_ID,
+          documentType: DOCUMENT_TYPE,
+        }).count();
+
+        (record = await subject.record({
+          serviceId: SERVICE_PROVIDER_ID,
+          documentType: DOCUMENT_TYPE,
+          content: REFILTERED_CONTENT,
+          mimeType: MIME_TYPE,
+          fetchDate: FETCH_DATE,
+          snapshotId: SNAPSHOT_ID,
+          isRefilter: true,
+        }));
+
+        numberOfRecordsAfter = await collection.find({
+          serviceId: SERVICE_PROVIDER_ID,
+          documentType: DOCUMENT_TYPE,
+        }).count();
+
+        ([mongoRecord] = await collection.find({
+          serviceId: SERVICE_PROVIDER_ID,
+          documentType: DOCUMENT_TYPE,
+        }).limit(1).sort({ created_at: -1 }).toArray());
+      });
+
+      after(() => collection.drop());
+
+      it('saves the record', () => {
+        expect(numberOfRecordsAfter).to.equal(numberOfRecordsBefore + 1);
+      });
+
+      it('returns the record id', () => {
+        expect(mongoRecord._id.toString()).to.equal(record.id);
+      });
+
+      it('returns a boolean to know if it is the first record', () => {
+        expect(record.isFirstRecord).to.be.false;
+      });
+
+      it('stores the service id', () => {
+        expect(mongoRecord.serviceId).to.include(SERVICE_PROVIDER_ID);
+      });
+
+      it('stores the document type', () => {
+        expect(mongoRecord.documentType).to.include(DOCUMENT_TYPE);
+      });
+
+      it('stores information that it is a refilter of this specific document', () => {
+        expect(mongoRecord.isRefilter).to.be.true;
+      });
+
+      it('stores the proper content', () => {
+        expect(mongoRecord.content).to.equal(REFILTERED_CONTENT);
+      });
+
+      context('when provided', () => {
+        it('stores the fetch date', () => {
+          expect(new Date(mongoRecord.fetchDate).getTime()).to.equal(FETCH_DATE.getTime());
+        });
+
+        it('stores the mime type', () => {
+          expect(mongoRecord.mimeType).to.equal(MIME_TYPE);
+        });
+
+        it('stores the snapshot ID', () => {
+          expect(mongoRecord.snapshotId.toString()).to.include(SNAPSHOT_ID);
+        });
+      });
+    });
+
+    context('with PDF document', () => {
+      before(async () => {
+        numberOfRecordsBefore = await collection.find({
+          serviceId: SERVICE_PROVIDER_ID,
+          documentType: DOCUMENT_TYPE,
+          content: PDF_CONTENT,
+          mimeType: PDF_MIME_TYPE,
+        }).count();
+
+        (record = await subject.record({
+          serviceId: SERVICE_PROVIDER_ID,
+          documentType: DOCUMENT_TYPE,
+          content: PDF_CONTENT,
+          mimeType: PDF_MIME_TYPE,
+          fetchDate: FETCH_DATE,
+          snapshotId: SNAPSHOT_ID,
+        }));
+
+        numberOfRecordsAfter = await collection.find({
+          serviceId: SERVICE_PROVIDER_ID,
+          documentType: DOCUMENT_TYPE,
+        }).count();
+
+        (mongoRecord = await collection.findOne({
+          serviceId: SERVICE_PROVIDER_ID,
+          documentType: DOCUMENT_TYPE,
+        }));
+      });
+
+      after(() => collection.drop());
+
+      it('saves the record', () => {
+        expect(numberOfRecordsAfter).to.equal(numberOfRecordsBefore + 1);
+      });
+
+      it('returns the record id', () => {
+        expect(mongoRecord._id.toString()).to.equal(record.id);
+      });
+
+      it('returns a boolean to know if it is the first record', () => {
+        expect(record.isFirstRecord).to.be.true;
+      });
+
+      it('stores the service id', () => {
+        expect(mongoRecord.serviceId).to.include(SERVICE_PROVIDER_ID);
+      });
+
+      it('stores the document type', () => {
+        expect(mongoRecord.documentType).to.include(DOCUMENT_TYPE);
+      });
+
+      it('stores information that it is the first record for this specific document', () => {
+        expect(mongoRecord.isFirstRecord).to.be.true;
+      });
+
+      it('stores the proper content', () => {
+        expect(mongoRecord.content).to.equal(PDF_CONTENT);
+      });
+
+      context('when provided', () => {
+        it('stores the fetch date', () => {
+          expect(new Date(mongoRecord.fetchDate).getTime()).to.equal(FETCH_DATE.getTime());
+        });
+
+        it('stores the mime type', () => {
+          expect(mongoRecord.mimeType).to.equal(PDF_MIME_TYPE);
+        });
+
+        it('stores the snapshot ID', () => {
+          expect(mongoRecord.snapshotId.toString()).to.include(SNAPSHOT_ID);
+        });
+      });
+    });
+  });
+
+  describe('#getLatestRecord', () => {
+    context('when there are records for the given service', () => {
+      let lastSnapshotId;
+      let latestRecord;
+
+      context('with HTML document', () => {
+        const UPDATED_CONTENT = `${CONTENT} (with additional content to trigger a record)`;
+
+        before(async () => {
+          await subject.record({
+            serviceId: SERVICE_PROVIDER_ID,
+            documentType: DOCUMENT_TYPE,
+            content: CONTENT,
+            fetchDate: FETCH_DATE,
+          });
+
+          ({ id: lastSnapshotId } = await subject.record({
+            serviceId: SERVICE_PROVIDER_ID,
+            documentType: DOCUMENT_TYPE,
+            content: UPDATED_CONTENT,
+            mimeType: MIME_TYPE,
+            fetchDate: FETCH_DATE_LATER,
+          }));
+
+          latestRecord = await subject.getLatestRecord(
+            SERVICE_PROVIDER_ID,
+            DOCUMENT_TYPE,
+          );
+        });
+
+        after(() => collection.drop());
+
+        it('returns the latest record id', async () => {
+          expect(latestRecord.id).to.include(lastSnapshotId);
+        });
+
+        it('returns the latest record content', async () => {
+          expect(latestRecord.content.toString('utf8')).to.equal(UPDATED_CONTENT);
+        });
+
+        it('returns the latest record mime type', async () => {
+          expect(latestRecord.mimeType).to.equal(MIME_TYPE);
+        });
+      });
+
+      context('with PDF document', () => {
+        before(async () => {
+          await subject.record({
+            serviceId: SERVICE_PROVIDER_ID,
+            documentType: DOCUMENT_TYPE,
+            content: PDF_CONTENT,
+            mimeType: PDF_MIME_TYPE,
+            fetchDate: FETCH_DATE,
+          });
+
+          ({ id: lastSnapshotId } = await subject.record({
+            serviceId: SERVICE_PROVIDER_ID,
+            documentType: DOCUMENT_TYPE,
+            content: UPDATED_PDF_CONTENT,
+            mimeType: PDF_MIME_TYPE,
+            fetchDate: FETCH_DATE_LATER,
+          }));
+
+          latestRecord = await subject.getLatestRecord(SERVICE_PROVIDER_ID, DOCUMENT_TYPE);
+        });
+
+        after(() => collection.drop());
+
+        it('returns the latest record id', async () => {
+          expect(latestRecord.id).to.include(lastSnapshotId);
+        });
+
+        it('returns the latest record content', async () => {
+          expect(latestRecord.content).to.equal(UPDATED_PDF_CONTENT);
+        });
+
+        it('returns the latest record mime type', async () => {
+          expect(latestRecord.mimeType).to.equal(PDF_MIME_TYPE);
+        });
+      });
+    });
+
+    context('when there are no records for the given service', () => {
+      let latestRecord;
+
+      before(async () => {
+        latestRecord = await subject.getLatestRecord(SERVICE_PROVIDER_ID, DOCUMENT_TYPE);
+      });
+
+      it('returns no id', async () => {
+        expect(latestRecord.id).to.be.null;
+      });
+
+      it('returns no content', async () => {
+        expect(latestRecord.content).to.be.null;
+      });
+
+      it('returns no mime type', async () => {
+        expect(latestRecord.mimeType).to.be.null;
+      });
+    });
+  });
+});
