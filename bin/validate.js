37eaed67ef6015ceb1ee07aca14b5e21a46f583e
Nicolas Dupont <npg.dupont@gmail.com>
2021-12-09 16:37:54 +0100

    Fix exit code on error on validate command

--- bin/validate.js
+++ bin/validate.js
@@ -25,17 +25,22 @@ const mocha = new Mocha({});
 const VALIDATE_PATH = path.resolve(__dirname, '../scripts/validation/validate.js');
 
 (async () => {
-  mocha.delay(); // As the validation script performs an asynchronous load before running the tests, the execution of the tests must be delayed. It works in addition to the `run` instruction after the loading has been done in the validation script.
-  mocha.addFile(VALIDATE_PATH); // As `delay` has been called, this statement will not load the file directly, `loadFilesAsync` is required.
-  await mocha.loadFilesAsync(); // Load files previously added to the Mocha cache with `addFile`.
+  try {
+    mocha.delay(); // As the validation script performs an asynchronous load before running the tests, the execution of the tests must be delayed. It works in addition to the `run` instruction after the loading has been done in the validation script.
+    mocha.addFile(VALIDATE_PATH); // As `delay` has been called, this statement will not load the file directly, `loadFilesAsync` is required.
+    await mocha.loadFilesAsync(); // Load files previously added to the Mocha cache with `addFile`.
 
-  let hasFailedTests = false;
+    let hasFailedTests = false;
 
-  mocha.run()
-    .on('fail', () => { hasFailedTests = true; })
-    .on('end', () => {
-      if (hasFailedTests) {
-        process.exitCode = 1;
-      }
-    });
+    mocha.run()
+      .on('fail', () => { hasFailedTests = true; })
+      .on('end', () => {
+        if (hasFailedTests) {
+          process.exitCode = 1;
+        }
+      });
+  } catch (error) {
+    console.error(error);
+    process.exitCode = 1;
+  }
 })();
