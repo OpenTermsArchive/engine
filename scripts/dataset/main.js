2a47d1630e0cf4c2a96ae7ca4509719b166495c7
Nicolas Dupont <npg.dupont@gmail.com>
2022-02-10 09:02:56 +0100

    Improve variable naming
    Co-authored-by: Matti Schneider <matti@opentermsarchive.org>

--- scripts/dataset/main.js
+++ scripts/dataset/main.js
@@ -4,14 +4,14 @@ import { release } from './index.js';
 
 (async () => {
   const args = process.argv.slice(2);
-  const publicationEnabled = args.includes('--publish');
-  const removeLocalCopyEnabled = args.includes('--remove-local-copy');
-  const scheduleEnable = args.includes('--schedule');
+  const shouldPublish = args.includes('--publish');
+  const shouldRemoveLocalCopy = args.includes('--remove-local-copy');
+  const shouldSchedule = args.includes('--schedule');
   const argsWithoutOptions = args.filter(arg => !arg.startsWith('--'));
   const [fileName] = argsWithoutOptions;
 
-  if (!scheduleEnable) {
-    return release({ publicationEnabled, removeLocalCopyEnabled, fileName });
+  if (!shouldSchedule) {
+    return release({ shouldPublish, shouldRemoveLocalCopy, fileName });
   }
 
   const RELEASE_HOURS_INTERVAL = 24 * 7;
@@ -20,7 +20,7 @@ import { release } from './index.js';
 
   setInterval(async () => {
     logger.info('Start creating the release…');
-    await release({ publicationEnabled, removeLocalCopyEnabled, fileName })();
+    await release({ shouldPublish, shouldRemoveLocalCopy, fileName })();
     logger.info('Release published');
   }, RELEASE_HOURS_INTERVAL * 60 * 60 * 1000);
 })();
