8c527b11264caf5aa4d2d231e8cf4293de4a8744
Nicolas Dupont <npg.dupont@gmail.com>
2022-02-10 09:09:14 +0100

    Send mail on dataset scripts errors

--- scripts/dataset/logger/index.js
+++ scripts/dataset/logger/index.js
@@ -1,4 +1,11 @@
+import 'winston-mail';
+import os from 'os';
+
+import config from 'config';
+import dotenv from 'dotenv';
 import winston from 'winston';
+
+dotenv.config();
 
 const { combine, timestamp, printf, colorize } = winston.format;
 
@@ -11,6 +18,26 @@ const alignedWithColorsAndTime = combine(
 const consoleTransport = new winston.transports.Console();
 
 const transports = [consoleTransport];
+
+if (config.get('logger.sendMailOnError')) {
+  const mailerOptions = {
+    to: config.get('logger.sendMailOnError.to'),
+    from: config.get('logger.sendMailOnError.from'),
+    host: config.get('logger.smtp.host'),
+    username: config.get('logger.smtp.username'),
+    password: process.env.SMTP_PASSWORD,
+    ssl: true,
+    timeout: 30 * 1000,
+    formatter: args => args[Object.getOwnPropertySymbols(args)[1]], // Returns the full error message, the same visible in the console. It is referenced in the argument object with a Symbol of which we do not have the reference but we know it is the second one.
+    exitOnError: true,
+  };
+
+  transports.push(new winston.transports.Mail({
+    ...mailerOptions,
+    level: 'error',
+    subject: `[OTA] Release Error Report — ${os.hostname()}`,
+  }));
+}
 
 const logger = winston.createLogger({
   format: alignedWithColorsAndTime,
