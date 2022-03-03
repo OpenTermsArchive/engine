ca2cdf01d6db1eed694fc7f97fa17af94187949e
Nicolas Dupont <npg.dupont@gmail.com>
2022-03-03 16:00:39 +0100

    Make MongoDB take precedence over Git storage config

--- src/index.js
+++ src/index.js
@@ -11,14 +11,14 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export function instantiateVersionsStorageAdapter() {
   let versionsStorageAdapter;
 
-  if (config.has('recorder.versions.storage.git')) {
+  if (config.has('recorder.versions.storage.mongo')) {
+    versionsStorageAdapter = new MongoAdapter(config.get('recorder.versions.storage.mongo'));
+  } else if (config.has('recorder.versions.storage.git')) {
     versionsStorageAdapter = new GitAdapter({
       ...config.get('recorder.versions.storage.git'),
       path: path.resolve(__dirname, '../', config.get('recorder.versions.storage.git.path')),
       fileExtension: 'md',
     });
-  } else if (config.has('recorder.versions.storage.mongo')) {
-    versionsStorageAdapter = new MongoAdapter(config.get('recorder.versions.storage.mongo'));
   } else {
     throw new Error('No config were found for versions storage adapter');
   }
@@ -29,14 +29,14 @@ export function instantiateVersionsStorageAdapter() {
 export function instantiateSnapshotsStorageAdapter() {
   let snapshotsStorageAdapter;
 
-  if (config.has('recorder.snapshots.storage.git')) {
+  if (config.has('recorder.snapshots.storage.mongo')) {
+    snapshotsStorageAdapter = new MongoAdapter(config.get('recorder.snapshots.storage.mongo'));
+  } else if (config.has('recorder.snapshots.storage.git')) {
     snapshotsStorageAdapter = new GitAdapter({
       ...config.get('recorder.snapshots.storage.git'),
       path: path.resolve(__dirname, '../', config.get('recorder.snapshots.storage.git.path')),
       fileExtension: 'html',
     });
-  } else if (config.has('recorder.snapshots.storage.mongo')) {
-    snapshotsStorageAdapter = new MongoAdapter(config.get('recorder.snapshots.storage.mongo'));
   } else {
     throw new Error('No config were found for snapshots storage adapter');
   }
