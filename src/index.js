7d25ef1158a213b6f449afdc494e393560cdd05a
Nicolas Dupont <npg.dupont@gmail.com>
2022-03-07 11:23:46 +0100

    Extract storage adapters instantiation from app main
    Co-authored-by: Matti Schneider <matti@opentermsarchive.org>

--- src/index.js
+++ src/index.js
@@ -3,81 +3,12 @@ import { fileURLToPath } from 'url';
 
 import config from 'config';
 
-import Archivist from './archivist/index.js';
-import logger from './logger/index.js';
-import Notifier from './notifier/index.js';
 import GitAdapter from './storage-adapters/git/index.js';
 import MongoAdapter from './storage-adapters/mongo/index.js';
-import Tracker from './tracker/index.js';
-
-const args = process.argv.slice(2);
-const refilterOnly = args.includes('--refilter-only');
-const schedule = args.includes('--schedule');
-const extraArgs = args.filter(arg => !arg.startsWith('--'));
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-(async function startOpenTermsArchive() {
-  const { versionsStorageAdapter, snapshotsStorageAdapter } = initStorageAdapters();
-
-  const archivist = new Archivist({
-    storage: {
-      versions: versionsStorageAdapter,
-      snapshots: snapshotsStorageAdapter,
-    },
-  });
-
-  archivist.attach(logger);
-
-  await archivist.initialize();
-
-  logger.info('Start Open Terms Archive\n');
-
-  let serviceIds;
-
-  if (extraArgs.length) {
-    serviceIds = extraArgs.filter(serviceId => {
-      const isServiceDeclared = archivist.serviceDeclarations[serviceId];
-
-      if (!isServiceDeclared) {
-        logger.warn(`Parameter "${serviceId}" was interpreted as a service ID to update, but no matching declaration was found; it will be ignored`);
-      }
-
-      return isServiceDeclared;
-    });
-  }
-
-  await archivist.refilterAndRecord(serviceIds);
-
-  if (refilterOnly) {
-    return;
-  }
-
-  if (process.env.NODE_ENV === 'production') {
-    archivist.attach(new Notifier(archivist.serviceDeclarations));
-  }
-
-  if (process.env.GITHUB_TOKEN) {
-    archivist.attach(new Tracker());
-  }
-
-  await archivist.trackChanges(serviceIds);
-
-  if (!schedule) {
-    return;
-  }
-
-  logger.info('The scheduler is running…');
-
-  const TRACK_CHANGES_HOURS_INTERVAL = 24;
-
-  logger.info(`Documents will be tracked every ${TRACK_CHANGES_HOURS_INTERVAL} hours\n`);
-  setInterval(async () => {
-    await archivist.trackChanges(serviceIds);
-  });
-}());
-
-function initStorageAdapters() {
+export function instantiateVersionsStorageAdapter() {
   let versionsStorageAdapter;
 
   if (config.has('recorder.versions.storage.git')) {
@@ -86,12 +17,16 @@ function initStorageAdapters() {
       path: path.resolve(__dirname, '../', config.get('recorder.versions.storage.git.path')),
       fileExtension: 'md',
     });
-  }
-
-  if (config.has('recorder.versions.storage.mongo')) {
+  } else if (config.has('recorder.versions.storage.mongo')) {
     versionsStorageAdapter = new MongoAdapter(config.get('recorder.versions.storage.mongo'));
+  } else {
+    throw new Error('No config were found for versions storage adapter');
   }
 
+  return versionsStorageAdapter;
+}
+
+export function instantiateSnapshotsStorageAdapter() {
   let snapshotsStorageAdapter;
 
   if (config.has('recorder.snapshots.storage.git')) {
@@ -100,14 +35,11 @@ function initStorageAdapters() {
       path: path.resolve(__dirname, '../', config.get('recorder.snapshots.storage.git.path')),
       fileExtension: 'html',
     });
-  }
-
-  if (config.has('recorder.snapshots.storage.mongo')) {
+  } else if (config.has('recorder.snapshots.storage.mongo')) {
     snapshotsStorageAdapter = new MongoAdapter(config.get('recorder.snapshots.storage.mongo'));
+  } else {
+    throw new Error('No config were found for snapshots storage adapter');
   }
 
-  return {
-    versionsStorageAdapter,
-    snapshotsStorageAdapter,
-  };
+  return snapshotsStorageAdapter;
 }
