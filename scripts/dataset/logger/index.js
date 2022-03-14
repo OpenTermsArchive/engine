7072b498070ab67d27638b156dc6770c69b81624
Nicolas Dupont <npg.dupont@gmail.com>
2022-03-14 15:06:44 +0100

    Simplify code
    Co-authored-by: Matti Schneider <matti@opentermsarchive.org>

--- scripts/dataset/logger/index.js
+++ scripts/dataset/logger/index.js
@@ -4,20 +4,14 @@ import logger from '../../../src/logger/index.js';
 
 const { combine, timestamp, printf, colorize } = winston.format;
 
-const alignedWithColorsAndTime = combine(
+logger.format = combine(
   colorize(),
   timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
   printf(({ level, message, counter, hash, timestamp }) => {
-    let prefix = '';
-
-    if (counter && hash) {
-      prefix = `${counter.toString().padEnd(6)} ${hash.padEnd(40)}`;
-    }
+    const prefix = counter && hash ? `${counter.toString().padEnd(6)} ${hash.padEnd(40)}` : '';
 
     return `${timestamp} ${level.padEnd(15)} ${prefix.padEnd(50)} ${message}`;
   }),
 );
-
-logger.format = alignedWithColorsAndTime;
 
 export default logger;
