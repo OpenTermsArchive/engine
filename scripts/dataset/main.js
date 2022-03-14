e39b7b46b70fb7bbcef2b1f5ee89a3354ec97d4c
Matti Schneider <gh@mattischneider.fr>
2022-03-14 15:57:53 +0100

    Refactor release script

--- scripts/dataset/main.js
+++ scripts/dataset/main.js
@@ -2,19 +2,19 @@ import { release } from './index.js';
 
 const RELEASE_HOURS_INTERVAL = 24;
 
-(() => {
-  const args = process.argv.slice(2);
-  const shouldPublish = args.includes('--publish');
-  const shouldRemoveLocalCopy = args.includes('--remove-local-copy');
-  const shouldSchedule = args.includes('--schedule');
-  const argsWithoutOptions = args.filter(arg => !arg.startsWith('--'));
-  const [fileName] = argsWithoutOptions;
+const args = process.argv.slice(2);
+const argsWithoutOptions = args.filter(arg => !arg.startsWith('--'));
+const [fileName] = argsWithoutOptions;
+const shouldSchedule = args.includes('--schedule');
 
-  if (!shouldSchedule) {
-    return release({ shouldPublish, shouldRemoveLocalCopy, fileName });
-  }
+const options = {
+  fileName,
+  shouldPublish: args.includes('--publish'),
+  shouldRemoveLocalCopy: args.includes('--remove-local-copy'),
+};
 
-  setInterval(async () => {
-    await release({ shouldPublish, shouldRemoveLocalCopy, fileName });
-  }, RELEASE_HOURS_INTERVAL * 60 * 60 * 1000);
-})();
+if (!shouldSchedule) {
+  release(options);
+} else {
+  setInterval(release, RELEASE_HOURS_INTERVAL * 60 * 60 * 1000, options);
+}
