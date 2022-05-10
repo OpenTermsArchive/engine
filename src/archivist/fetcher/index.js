2ffe178332360f9b1b041441f38ede510d1fd902
Martin Ratinaud <martinratinaud@gmail.com>
2022-05-10 10:54:40 +0400

    Force passing of options to fetcher

--- src/archivist/fetcher/index.js
+++ src/archivist/fetcher/index.js
@@ -4,10 +4,10 @@ import fetchHtmlOnly from './htmlOnlyFetcher.js';
 export { launchHeadlessBrowser, stopHeadlessBrowser } from './fullDomFetcher.js';
 export { FetchDocumentError } from './errors.js';
 
-export default async function fetch({ url, executeClientScripts, cssSelectors }) {
+export default async function fetch({ url, executeClientScripts, cssSelectors, options }) {
   if (executeClientScripts) {
-    return fetchFullDom(url, cssSelectors);
+    return fetchFullDom(url, cssSelectors, options);
   }
 
-  return fetchHtmlOnly(url);
+  return fetchHtmlOnly(url, options);
 }
