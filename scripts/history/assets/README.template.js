0332a4c4c4b76996c22d68bafc52819af84fa005
Nicolas Dupont <npg.dupont@gmail.com>
2022-05-11 10:57:00 +0200

    Add history scripts README assets

---
+++ scripts/history/assets/README.template.js
@@ -0,0 +1,13 @@
+export default function readme({ name }) {
+  return `# Open Terms Archive - ${name}
+
+This repository is to be considered as a database only.
+You can find the documentation by following the link in the “About” section of this page.
+
+# License
+
+Data in this repository is distributed under an ODC-BY 1.0 license. That means you are free to share (to copy, distribute and use the database), to create (to produce works from the database), to adapt (to modify, transform and build upon the database) as long as you attribute the resulting works to _Open Terms Archive with contributions from HestiaLabs_.
+
+Contact the author if you have any specific need or question regarding licensing.
+`;
+}
