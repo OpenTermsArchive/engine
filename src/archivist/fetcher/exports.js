3d57b52ee9ca618188c5dc871fc6e0a1cc1da19b
Nicolas Dupont <npg.dupont@gmail.com>
2022-05-24 15:12:22 +0200

    Move Fetcher default config in exports.js
    This allow to set defaults values for fetcher module only when imported
    outside OTA

--- src/archivist/fetcher/exports.js
+++ src/archivist/fetcher/exports.js
@@ -1,4 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import config from 'config';
+
 import fetcher from './index.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const defaultConfigs = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../../config/default.json')));
+
+config.util.setModuleDefaults('fetcher', defaultConfigs.fetcher);
 
 export { launchHeadlessBrowser, stopHeadlessBrowser } from './index.js';
 
