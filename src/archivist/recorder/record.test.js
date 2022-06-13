f0beb5f786624d499f25f6049c65c3225c88f72d
Nicolas Dupont <npg.dupont@gmail.com>
2022-06-13 17:39:19 +0200

    Improve Record test readability

--- src/archivist/recorder/record.test.js
+++ src/archivist/recorder/record.test.js
@@ -59,17 +59,33 @@ describe('Record', () => {
     });
   });
 
-  context('when trying to access content and it is neither defined nor loaded', () => {
-    it('throws an error explaining how to recover', async () => {
-      try {
-        console.log(record.content);
-      } catch (e) {
-        expect(e).to.be.an('error');
-        expect(e.message).to.have.string('set the content or use Repository#loadRecordContent');
+  describe('Content access', () => {
+    before(async () => {
+      repository = await (RepositoryFactory.create(config.get('recorder.versions.storage'))).initialize();
+      await repository.save(new Record({
+        ...recordParams,
+        content: 'content',
+      }));
+      ([record] = await repository.findAll());
+    });
 
-        return;
-      }
-      expect.fail('No error was thrown');
+    after(async () => {
+      await repository.removeAll();
+      await repository.finalize();
+    });
+
+    context('when it is neither defined nor loaded', () => {
+      it('throws an error explaining how to recover', async () => {
+        try {
+          console.log(record.content);
+        } catch (e) {
+          expect(e).to.be.an('error');
+          expect(e.message).to.have.string('set the content or use Repository#loadRecordContent');
+
+          return;
+        }
+        expect.fail('No error was thrown');
+      });
     });
   });
 });
