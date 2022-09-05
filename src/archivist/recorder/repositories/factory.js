41af10cba937caa19976f7f182c98f5f03768899
Matti Schneider <matti@opentermsarchive.org>
2022-09-05 16:18:05 +0200

    Refactor RepositoryFactory

--- src/archivist/recorder/repositories/factory.js
+++ src/archivist/recorder/repositories/factory.js
@@ -4,17 +4,21 @@ import GitRepository from './git/index.js';
 import MongoRepository from './mongo/index.js';
 
 export default class RepositoryFactory {
-  static create(params) {
-    switch (params.type) {
+  /**
+   * @param {Object} storageConfig   A configuration object describing which repository type should be created from the `config.recorder.$recordType.storage` configuration.
+   * @returns {RepositoryInterface}
+   */
+  static create(storageConfig) {
+    switch (storageConfig.type) {
     case 'git':
       return new GitRepository({
-        ...params.git,
-        path: path.resolve(process.cwd(), params.git.path),
+        ...storageConfig.git,
+        path: path.resolve(process.cwd(), storageConfig.git.path),
       });
     case 'mongo':
-      return new MongoRepository(params.mongo);
+      return new MongoRepository(storageConfig.mongo);
     default:
-      throw new Error(`Unknown storage repository configuration for type '${params.type}'`);
+      throw new Error(`Unknown storage repository configuration for type '${storageConfig.type}'`);
     }
   }
 }
