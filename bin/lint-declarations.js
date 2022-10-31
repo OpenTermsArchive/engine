c6c5d199126c68fda6ab4f51ac04d53974574f2e
Martin Ratinaud <martinratinaud@gmail.com>
2022-10-31 08:52:30 +0400

    Fix lint declaration program name and description

--- bin/lint-declarations.js
+++ bin/lint-declarations.js
@@ -1,4 +1,7 @@
 #! /usr/bin/env node
+// makes it easy to lint all files relative to one service ID, which would have been
+// more difficult to achieve using an eslint based command directly defined in the package.json.
+// It also ensures that the same version of eslint is used in the OpenTermsArchive core and declarations repositories.
 import './.env.js'; // Workaround to ensure `SUPPRESS_NO_CONFIG_WARNING` is set before config is imported
 
 import fs from 'fs';
@@ -16,11 +19,8 @@ config.util.setModuleDefaults('services', { declarationsPath: path.resolve(proce
 const { version } = JSON.parse(fs.readFileSync(new URL('../package.json', import.meta.url)).toString());
 
 program
-  .name('lint declaration files')
-  .description(`makes it easy to lint all files relative to one service ID, which would have been
-more difficult to achieve using an eslint based command directly defined in the package.json.
-
-It also ensures that the same version of eslint is used in the OpenTermsArchive core and declarations repositories.`)
+  .name('ota-lint-declarations')
+  .description('Check format and stylistic errors in declarations and auto fix them')
   .version(version)
   .option('-s, --services [serviceId...]', 'service IDs of services to handle')
   .option('-m, --modified', 'to only lint modified services already commited to git');
