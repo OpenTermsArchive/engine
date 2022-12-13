1af26ae913939f59f9dc776bd72f7be7567adbad
Matti Schneider <matti@opentermsarchive.org>
2022-12-13 15:32:34 +0700

    Revert test to check for regressions

--- src/archivist/filter/index.test.js
+++ src/archivist/filter/index.test.js
@@ -62,7 +62,7 @@ const rawHTMLWithCommonChangingItems = `
     <p><a id="link2" href="#anchor">link 2</a></p>
     <p><a id="link3" href="http://absolute.url/link">link 3</a></p>
     <p><a id="link4" href="">link 4</a></p>
-    <p><a href="/cdn-cgi/l/email-protection#3b4c52555f484f495e5a56154b49524d5a584215484f5a4f5e565e554f7b4c52555f484f495e5a5615585456">[email&#160;protected]</a></p>
+    <a href="/cdn-cgi/l/email-protection#3b4c52555f484f495e5a56154b49524d5a584215484f5a4f5e565e554f7b4c52555f484f495e5a5615585456">[email&#160;protected]</a>
     <p><a href="/cdn-cgi/l/email-protection#2d4e4243594c4e596d4e4459545e4e424259034858">conta<span>[email&#160;protected]</span></a></p>
   </body>
 </html>`;
