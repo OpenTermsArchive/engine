93b7e5e5a866351a59ae88044a493586f822735f
Martin Ratinaud <martinratinaud@gmail.com>
2023-02-01 11:07:27 +0400

    Update ota lint function to use new mocha test file

--- bin/ota-lint.js
+++ bin/ota-lint.js
@@ -2,11 +2,21 @@
 import './env.js';
 
 import path from 'path';
-import { fileURLToPath, pathToFileURL } from 'url';
+import { fileURLToPath } from 'url';
 
 import { program } from 'commander';
+import Mocha from 'mocha';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const LINT_TEST_FILEPATH = '../scripts/declarations/lint/index.mocha.js';
+const LINT_PATH = path.resolve(__dirname, LINT_TEST_FILEPATH);
+
+// Mocha catches unhandled rejection from the user code and re-emits them to the process (see https://github.com/mochajs/mocha/blob/master/lib/runner.js#L198)
+process.on('unhandledRejection', reason => {
+  // Re-throw them so that the validation command fails in these cases (for example, if there is a syntax error when parsing JSON declaration files)
+  throw reason;
+});
 
 program
   .name('ota lint')
@@ -14,6 +24,32 @@ program
   .option('-s, --services [serviceId...]', 'service IDs of services to lint')
   .option('-m, --modified', 'to only lint modified services already commited to git');
 
-const lintDeclarations = (await import(pathToFileURL(path.resolve(__dirname, '../scripts/declarations/lint/index.js')))).default; // load asynchronously to ensure env.js is loaded before
+const mocha = new Mocha({
+  delay: true, // as the validation script performs an asynchronous load before running the tests, the execution of the tests are delayed until run() is called
+  failZero: true, // consider that being called with no service to validate is a failure
+});
 
-lintDeclarations(program.parse().opts());
+(async () => {
+  mocha.addFile(LINT_PATH); // As `delay` has been called, this statement will not load the file directly, `loadFilesAsync` is required.
+  await mocha.loadFilesAsync() // Load files previously added to the Mocha cache with `addFile`.
+    .catch(error => {
+      console.error(error);
+      process.exit(2);
+    });
+
+  let hasFailedTests = false;
+
+  const lintFiles = (await import(LINT_TEST_FILEPATH)).default;
+
+  lintFiles(program.parse().opts());
+
+  mocha.run()
+    .on('fail', () => { hasFailedTests = true; })
+    .on('end', () => {
+      if (hasFailedTests) {
+        process.exit(1);
+      }
+
+      process.exit(0);
+    });
+})();
