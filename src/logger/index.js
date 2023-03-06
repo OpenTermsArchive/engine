6104881ae96cba96b3087f36a35024aee2861c3d
Nicolas Dupont <npg.dupont@gmail.com>
2023-03-06 15:25:33 +0100

    Fix vocabulary in logger

--- src/logger/index.js
+++ src/logger/index.js
@@ -99,22 +99,22 @@ logger.onVersionNotChanged = (serviceId, type) => {
   logger.info({ message: 'No changes after filtering, did not record version', serviceId, type });
 };
 
-logger.onTrackingStarted = (numberOfServices, numberOfDocuments, extractOnly) => {
+logger.onTrackingStarted = (numberOfServices, numberOfTerms, extractOnly) => {
   if (extractOnly) {
-    logger.info(`Examining ${numberOfDocuments} documents from ${numberOfServices} services for extraction…`);
+    logger.info(`Examining ${numberOfTerms} terms from ${numberOfServices} services for extraction…`);
   } else {
-    logger.info(`Tracking changes of ${numberOfDocuments} documents from ${numberOfServices} services…`);
+    logger.info(`Tracking changes of ${numberOfTerms} terms from ${numberOfServices} services…`);
   }
   recordedSnapshotsCount = 0;
   recordedVersionsCount = 0;
 };
 
-logger.onTrackingCompleted = (numberOfServices, numberOfDocuments, extractOnly) => {
+logger.onTrackingCompleted = (numberOfServices, numberOfTerms, extractOnly) => {
   if (extractOnly) {
-    logger.info(`Examined ${numberOfDocuments} documents from ${numberOfServices} services for extraction`);
+    logger.info(`Examined ${numberOfTerms} terms from ${numberOfServices} services for extraction`);
     logger.info(`Recorded ${recordedVersionsCount} new versions\n`);
   } else {
-    logger.info(`Tracked changes of ${numberOfDocuments} documents from ${numberOfServices} services`);
+    logger.info(`Tracked changes of ${numberOfTerms} terms from ${numberOfServices} services`);
     logger.info(`Recorded ${recordedSnapshotsCount} new snapshots and ${recordedVersionsCount} new versions\n`);
   }
 };
