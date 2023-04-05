350043c2507b07123788d869cb8e563a6e3944e4
Nicolas Dupont <npg.dupont@gmail.com>
2023-04-05 11:13:13 +0200

    Create a logger for API server

---
+++ src/api/logger.js
@@ -0,0 +1,17 @@
+import winston from 'winston';
+
+const { combine, timestamp, printf, colorize } = winston.format;
+
+const transports = [new winston.transports.Console()];
+
+const logger = winston.createLogger({
+  format: combine(
+    colorize(),
+    timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    printf(({ level, message, timestamp }) => `${timestamp} ${level.padEnd(15)} ${message}`),
+  ),
+  transports,
+  rejectionHandlers: transports,
+});
+
+export default logger;
--- src/api/server.js
+++ src/api/server.js
@@ -2,9 +2,11 @@ import config from 'config';
 import express from 'express';
 import helmet from 'helmet';
 
+import logger from './logger.js';
+
 const app = express();
 
 app.use(helmet());
 app.listen(config.get('api.port'));
 
-console.debug(`Server listening on port ${config.get('api.port')}`);
+logger.info('Start Open Terms Archive Collection metadata API\n');
