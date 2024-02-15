baaa6458d726c2e3776ade3d275e8788bdaf4b75
Nicolas Dupont <npg.dupont@gmail.com>
2024-02-15 10:48:58 +0100

    Warn on Logger mailer disabled due to missing config

--- src/logger/index.js
+++ src/logger/index.js
@@ -34,42 +34,53 @@ const consoleTransport = new winston.transports.Console();
 
 const transports = [consoleTransport];
 
-if (process.env.SMTP_PASSWORD && config.get('logger.sendMailOnError')) {
-  const mailerOptions = {
-    to: config.get('logger.sendMailOnError.to'),
-    from: config.get('logger.sendMailOnError.from'),
-    host: config.get('logger.smtp.host'),
-    username: config.get('logger.smtp.username'),
-    password: process.env.SMTP_PASSWORD,
-    ssl: true,
-    timeout: 30 * 1000,
-    formatter: args => args[Object.getOwnPropertySymbols(args)[1]], // Returns the full error message, the same visible in the console. It is referenced in the argument object with a Symbol of which we do not have the reference but we know it is the second one.
-    exitOnError: true,
-  };
-
-  transports.push(new winston.transports.Mail({
-    ...mailerOptions,
-    level: 'error',
-    subject: `[OTA] Error Report — ${os.hostname()}`,
-  }));
-
-  if (config.get('logger.sendMailOnError.sendWarnings')) {
-    transports.push(new winston.transports.Mail({
-      ...mailerOptions,
-      level: 'warn',
-      subject: `[OTA] Inaccessible content — ${os.hostname()}`,
-    }));
-  }
-}
-
-let recordedSnapshotsCount;
-let recordedVersionsCount;
-
 const logger = winston.createLogger({
   format: alignedWithColorsAndTime,
   transports,
   rejectionHandlers: transports,
 });
+
+if (process.env.SMTP_PASSWORD) {
+  if (config.get('logger.sendMailOnError')) {
+    const mailerOptions = {
+      to: config.get('logger.sendMailOnError.to'),
+      from: config.get('logger.sendMailOnError.from'),
+      host: config.get('logger.smtp.host'),
+      username: config.get('logger.smtp.username'),
+      password: process.env.SMTP_PASSWORD,
+      ssl: true,
+      timeout: 30 * 1000,
+      formatter: args => args[Object.getOwnPropertySymbols(args)[1]], // Returns the full error message, the same visible in the console. It is referenced in the argument object with a Symbol of which we do not have the reference but we know it is the second one.
+      exitOnError: true,
+    };
+
+    transports.push(new winston.transports.Mail({
+      ...mailerOptions,
+      level: 'error',
+      subject: `[OTA] Error Report — ${os.hostname()}`,
+    }));
+
+    if (config.get('logger.sendMailOnError.sendWarnings')) {
+      transports.push(new winston.transports.Mail({
+        ...mailerOptions,
+        level: 'warn',
+        subject: `[OTA] Inaccessible content — ${os.hostname()}`,
+      }));
+    }
+
+    logger.configure({
+      transports,
+      rejectionHandlers: transports,
+    });
+  } else {
+    logger.warn('Configuration key "logger.sendMailOnError" was not found; the mailer feature of the Logger module won\'t be enabled');
+  }
+} else {
+  logger.warn('Environment variable "SMTP_PASSWORD" was not found; the mailer feature of the Logger module won\'t be enabled');
+}
+
+let recordedSnapshotsCount;
+let recordedVersionsCount;
 
 logger.onFirstSnapshotRecorded = ({ serviceId, termsType, documentId, id }) => {
   logger.info({ message: `Recorded first snapshot with id ${id}`, serviceId, termsType, documentId });
