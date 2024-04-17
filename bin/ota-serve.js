81a95f165a71076aaa41e5088c975810b8af8abd
Nicolas Dupont <npg.dupont@gmail.com>
2024-04-17 14:43:21 +0200

    wip

--- bin/ota-serve.js
+++ bin/ota-serve.js
@@ -6,4 +6,4 @@ import { fileURLToPath, pathToFileURL } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-await import(pathToFileURL(path.resolve(__dirname, '../src/api/server.js'))); // load asynchronously to ensure env.js is loaded before
+await import(pathToFileURL(path.resolve(__dirname, '../src/collection-api/server.js'))); // load asynchronously to ensure env.js is loaded before
