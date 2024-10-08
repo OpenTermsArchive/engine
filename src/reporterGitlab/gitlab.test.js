46567ed3ee3d764d81a988fed20d06c6f249df67
ZAGO Alessandro <alessandro.zago@ext.ec.europa.eu>
2024-10-08 14:33:44 +0200

    Add EUPL-1.2 copyright

--- src/reporterGitlab/gitlab.test.js
+++ src/reporterGitlab/gitlab.test.js
@@ -1,3 +1,19 @@
+// Copyright (c) 2024 European Union
+// *
+// Licensed under the EUPL, Version 1.2 or – as soon they will be approved by the
+// European Commission – subsequent versions of the EUPL (the “Licence”);
+// You may not use this work except in compliance with the Licence.
+// You may obtain a copy of the Licence at:
+// *
+// https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+// *
+// Unless required by applicable law or agreed to in writing, software distributed under
+// the Licence is distributed on an “AS IS” basis, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, either express or implied. See the Licence for the specific language
+// governing permissions and limitations under the Licence.
+//
+// EUPL text (EUPL-1.2)
+
 import { createRequire } from 'module';
 
 import { expect } from 'chai';
