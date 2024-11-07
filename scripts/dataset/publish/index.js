aed17f341523d3686aa819e2c7e1dd7947114d67
Nicolas Dupont <npg.dupont@gmail.com>
2024-11-07 14:12:34 +0100

    Clarify token precedence between GitHub and GitLab

--- scripts/dataset/publish/index.js
+++ scripts/dataset/publish/index.js
@@ -2,6 +2,7 @@ import publishGitHub from './github/index.js';
 import publishGitLab from './gitlab/index.js';
 
 export default function publishRelease({ archivePath, releaseDate, stats }) {
+  // If both GitHub and GitLab tokens are defined, GitHub takes precedence
   if (process.env.OTA_ENGINE_GITHUB_TOKEN) {
     return publishGitHub({ archivePath, releaseDate, stats });
   }
