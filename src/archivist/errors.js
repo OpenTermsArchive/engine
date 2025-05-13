1d65dff3bc4a538978cf11e273d17ef35127e8e4
Nicolas Dupont <npg.dupont@gmail.com>
2025-05-13 10:58:54 +0200

    Use more modern idiomatic JS code

--- src/archivist/errors.js
+++ src/archivist/errors.js
@@ -1,11 +1,11 @@
 export class InaccessibleContentError extends Error {
-  constructor(errorOrErrors) {
-    const errors = [].concat(errorOrErrors);
-    const reasons = errors.map(error => (error instanceof Error ? error.message : error));
+  constructor(errors) {
+    const errorsArray = Array.isArray(errors) ? errors : [errors];
+    const reasons = errorsArray.map(error => (error instanceof Error ? error.message : String(error)));
 
     super(`The documents cannot be accessed or their contents can not be selected:${`\n - ${reasons.join('\n - ')}`}`);
     this.name = 'InaccessibleContentError';
     this.reasons = reasons;
-    this.errors = errors;
+    this.errors = errorsArray;
   }
 }
