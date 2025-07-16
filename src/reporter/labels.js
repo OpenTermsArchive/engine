389ef91b25d7605e8086e04bb52e5cfdf5c527f5
Nicolas Dupont <npg.dupont@gmail.com>
2025-07-16 15:38:18 +0200

    Sort labels

--- src/reporter/labels.js
+++ src/reporter/labels.js
@@ -3,6 +3,41 @@ export const DEPRECATED_MANAGED_BY_OTA_MARKER = '[managed by OTA]';
 export const MANAGED_BY_OTA_MARKER = '- Auto-managed by OTA engine';
 
 export const LABELS = {
+  DNS_LOOKUP_FAILURE: {
+    name: 'domain lookup failed',
+    color: 'FFFFFF',
+    description: 'Fetching failed because the domain temporarily failed to resolve on DNS',
+  },
+  DNS_RESOLUTION_FAILURE: {
+    name: 'domain resolution failed',
+    color: 'FFFFFF',
+    description: 'Fetching failed because the domain fails to resolve on DNS',
+  },
+  DOCUMENT_LOAD_TIMEOUT: {
+    name: 'document load timed out',
+    color: 'FFFFFF',
+    description: 'Fetching failed with a timeout error',
+  },
+  DOCUMENT_NOT_FOUND: {
+    name: 'document not found',
+    color: 'FFFFFF',
+    description: 'Fetch location is outdated',
+  },
+  DOCUMENT_STRUCTURE_CHANGE: {
+    name: 'document structure changed',
+    color: 'FFFFFF',
+    description: 'Extraction selectors are outdated',
+  },
+  EMPTY_CONTENT: {
+    name: 'document was empty',
+    color: 'FFFFFF',
+    description: 'Fetching failed because the server returns an empty content',
+  },
+  EMPTY_RESPONSE: {
+    name: 'received empty response',
+    color: 'FFFFFF',
+    description: 'Fetching failed because server returned an empty response body',
+  },
   HTTP_403: {
     name: 'document access forbidden',
     color: 'FFFFFF',
@@ -28,25 +63,15 @@ export const LABELS = {
     color: 'FFFFFF',
     description: 'Fetching failed with a 503 (service unavailable) HTTP code',
   },
+  INVALID_SELECTOR: {
+    name: 'invalid selector',
+    color: 'FFFFFF',
+    description: 'Some selectors cannot be understood by the engine',
+  },
   SSL_EXPIRED: {
     name: 'certificate expired',
     color: 'FFFFFF',
     description: 'Fetching failed because the domain SSL certificate has expired',
-  },
-  DNS_LOOKUP_FAILURE: {
-    name: 'domain lookup failed',
-    color: 'FFFFFF',
-    description: 'Fetching failed because the domain temporarily failed to resolve on DNS',
-  },
-  DNS_RESOLUTION_FAILURE: {
-    name: 'domain resolution failed',
-    color: 'FFFFFF',
-    description: 'Fetching failed because the domain fails to resolve on DNS',
-  },
-  EMPTY_RESPONSE: {
-    name: 'received empty response',
-    color: 'FFFFFF',
-    description: 'Fetching failed because server returned an empty response body',
   },
   SSL_INVALID: {
     name: 'certificate was invalid',
@@ -58,35 +83,10 @@ export const LABELS = {
     color: 'FFFFFF',
     description: 'Fetching failed because of too many redirects',
   },
-  DOCUMENT_LOAD_TIMEOUT: {
-    name: 'document load timed out',
-    color: 'FFFFFF',
-    description: 'Fetching failed with a timeout error',
-  },
-  EMPTY_CONTENT: {
-    name: 'document was empty',
-    color: 'FFFFFF',
-    description: 'Fetching failed because the server returns an empty content',
-  },
   UNKNOWN_FAILURE: {
     name: 'failed for unknown reason',
     color: 'FFFFFF',
     description: 'Fetching failed for an undetermined reason that needs investigation',
-  },
-  DOCUMENT_STRUCTURE_CHANGE: {
-    name: 'document structure changed',
-    color: 'FFFFFF',
-    description: 'Extraction selectors are outdated',
-  },
-  DOCUMENT_NOT_FOUND: {
-    name: 'document not found',
-    color: 'FFFFFF',
-    description: 'Fetch location is outdated',
-  },
-  INVALID_SELECTOR: {
-    name: 'invalid selector',
-    color: 'FFFFFF',
-    description: 'Some selectors cannot be understood by the engine',
   },
   NEEDS_INTERVENTION: {
     name: '⚠ needs intervention',
